1fefe87a5bbf0b3740cd7554759498723872d21b
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-06-28 13:39:31 +0100

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TAC_COM
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
