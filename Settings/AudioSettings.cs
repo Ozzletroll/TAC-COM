2e887f956cb315fd42ca4167d3fb321bd74560d4
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-08-06 16:32:41 +0100

    Refactor into UpdateAppConfig method

--- Settings/AudioSettings.cs
+++ Settings/AudioSettings.cs
@@ -28,5 +28,35 @@ namespace TAC_COM.Settings
                 this["outputDevice"] = value;
             }
         }
+
+        [ConfigurationProperty("noiseGateThreshold")]
+        public float NoiseGateThreshold
+        {
+            get => (float)this["noiseGateThreshold"];
+            set
+            {
+                this["noiseGateThreshold"] = value;
+            }
+        }
+
+        [ConfigurationProperty("outputLevel")]
+        public float OutputLevel
+        {
+            get => (float)this["outputLevel"];
+            set
+            {
+                this["outputLevel"] = value;
+            }
+        }
+
+        [ConfigurationProperty("interferenceLevel")]
+        public float InterferenceLevel
+        {
+            get => (float)this["interferenceLevel"];
+            set
+            {
+                this["interferenceLevel"] = value;
+            }
+        }
     }
 }
