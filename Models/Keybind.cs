d95cd444a8f7dbc28e66585341343302195b3e59
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-08-23 18:42:34 +0100

    Add keybind class

---
+++ Models/Keybind.cs
@@ -0,0 +1,16 @@
+﻿using Dapplo.Windows.Input.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAC_COM.Models
+{
+    internal class Keybind(VirtualKeyCode keyCode, bool shift, bool ctrl)
+    {
+        public bool Shift = shift;
+        public bool Ctrl = ctrl;
+        public VirtualKeyCode KeyCode = keyCode;
+    }
+}
