ec82dff70ba8209511a236a4274c4c111c427be7
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-08-30 15:41:39 +0100

    Implement adonisUI custom title bar

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using AdonisUI.Controls;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,7 +17,7 @@ namespace TAC_COM
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : AdonisWindow
     {
         public MainWindow()
         {
