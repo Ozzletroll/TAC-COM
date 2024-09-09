9241b4d2d7fb3121e25b72be6ed11e60dab61cd8
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-09-09 15:16:39 +0100

    Add placeholder icon

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using AdonisUI.Controls;
+using System.Drawing;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -24,6 +26,5 @@ namespace TAC_COM
             InitializeComponent();
             DataContext = new MainViewModel();
         }
-
     }
 }
