25cbca138f74d37ee74a5ef2fcbcb08db9e8f2a0
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-09-10 16:30:12 +0100

    Update keybind window to use AdonisUI

--- Views/KeybindWindow.xaml.cs
+++ Views/KeybindWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AdonisUI.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -21,12 +23,42 @@ namespace TAC_COM.Views
     /// <summary>
     /// Interaction logic for KeybindWindow.xaml
     /// </summary>
-    public partial class KeybindWindow : Window
+    public partial class KeybindWindow : AdonisWindow
     {
         public KeybindWindow(KeybindManager keybindManager)
         {
             InitializeComponent();
             DataContext = new KeybindWindowViewModel(keybindManager);
+
+            // Prevent moving of window
+            SourceInitialized += Window_SourceInitialized;
+        }
+
+        private void Window_SourceInitialized(object? sender, EventArgs e)
+        {
+            WindowInteropHelper helper = new(this);
+            HwndSource source = HwndSource.FromHwnd(helper.Handle);
+            source.AddHook(WndProc);
+        }
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            const int WM_SYSCOMMAND = 0x0112;
+            const int SC_MOVE = 0xF010;
+
+            switch (msg)
+            {
+                case WM_SYSCOMMAND:
+                    int command = wParam.ToInt32() & 0xfff0;
+                    if (command == SC_MOVE)
+                    {
+                        handled = true;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return IntPtr.Zero;
         }
     }
 }
