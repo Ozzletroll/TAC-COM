9e51c715739a6c0ed2d5f4de5d7e587e6cb685c1
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-09-12 17:24:15 +0100

    Render dial value to outer ring

--- Controls/Dial.xaml.cs
+++ Controls/Dial.xaml.cs
@@ -20,6 +20,62 @@ namespace TAC_COM.Controls
     /// </summary>
     public partial class Dial : UserControl
     {
+        private const double FULLRANGE_ANGLE = 270;
+        private const double START_ANGLE_OFFSET = 90;
+
+        public static readonly DependencyProperty MinProperty 
+            = DependencyProperty.Register("Min", typeof(int), typeof(Dial));
+        public int Min
+        {
+            get => (int)GetValue(MinProperty); 
+            set 
+            { 
+                SetValue(MinProperty, value); 
+            }
+        }
+
+        public static readonly DependencyProperty MaxProperty 
+            = DependencyProperty.Register("Max", typeof(int), typeof(Dial));
+        public int Max
+        {
+            get => (int)GetValue(MaxProperty);
+            set 
+            { 
+                SetValue(MaxProperty, value); 
+            }
+        }
+
+        public static readonly DependencyProperty ValueProperty 
+            = DependencyProperty.Register("Value", typeof(int), typeof(Dial), new FrameworkPropertyMetadata(0, new PropertyChangedCallback(OnValuePropertyChanged)));
+        public int Value
+        {
+            get => (int)GetValue(ValueProperty);
+            set 
+            { 
+                SetValue(ValueProperty, value); 
+            }
+        }
+        private static void OnValuePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            Dial? f = d as Dial;
+            f.Value = (int)e.NewValue;
+            f.RenderDisplay();
+        }
+
+        private void RenderDisplay()
+        {
+            // Draw dial outer ring display
+            int offset = 190;
+            double angle = FULLRANGE_ANGLE / (Max - Min) * (Value - Min);
+            double rad = (Math.PI / 180) * (angle + START_ANGLE_OFFSET);
+
+            double x = offset * Math.Cos(rad) + offset;
+            double y = offset * Math.Sin(rad) + offset;
+
+            display.IsLargeArc = angle > 180;
+            display.Point = new Point(x, y);
+        }
+
         public Dial()
         {
             InitializeComponent();
