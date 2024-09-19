48fa57748fffde20d7cf1aca16e65a649d2f7210
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-09-19 19:14:20 +0100

    Add IconService

---
+++ Services/IconService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAC_COM.Services
+{
+    internal class IconService(MainWindow? mainWindow)
+    {
+        private readonly MainWindow? mainWindow = mainWindow;
+
+        public void SetLiveIcon()
+        {
+            mainWindow?.ChangeNotifyIcon("./Static/Icons/live.ico", "Live");
+        }
+
+        public void SetEnabledIcon()
+        {
+            mainWindow?.ChangeNotifyIcon("./Static/Icons/enabled.ico", "Enabled");
+        }
+
+        public void SetStandbyIcon()
+        {
+            mainWindow?.ChangeNotifyIcon("./Static/Icons/standby.ico", "Standby");
+        }
+    }
+}
