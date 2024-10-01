6d1f9b7d469b666833bc658e931fc009fee6caf4
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-10-01 18:56:28 +0100

    Add theme changing via ThemeService

--- Services/ThemeService.cs
+++ Services/ThemeService.cs
@@ -10,9 +10,24 @@ namespace TAC_COM.Services
 {
     public class ThemeService
     {
-        public static void ChangeTheme(Uri currentTheme, Uri targetTheme)
+        private static ResourceDictionary? CurrentTheme;
+
+        public static void ChangeTheme(Uri targetTheme)
         {
-            ResourceLocator.SetColorScheme(Application.Current.Resources, currentTheme, targetTheme);
+            // Force refresh of resources.xaml
+            Application.Current.Resources.Clear();
+            Application.Current.Resources = new ResourceDictionary() { Source = new Uri("pack://application:,,,/Resources.xaml") };
+
+            ResourceDictionary rootResourceDictionary = Application.Current.Resources;
+            if (CurrentTheme != null)
+            {
+                rootResourceDictionary.MergedDictionaries.Remove(CurrentTheme);
+            }
+
+            ResourceDictionary TargetTheme = new() { Source = targetTheme };
+            rootResourceDictionary.MergedDictionaries.Add(TargetTheme);
+
+            CurrentTheme = TargetTheme;
         }
     }
 }
