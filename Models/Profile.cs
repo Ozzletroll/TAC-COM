9b2d77b959c8cfc499f13d21cafc798c73d4b92f
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-10-01 11:42:51 +0100

    Update profile to use themes

--- Models/Profile.cs
+++ Models/Profile.cs
@@ -3,16 +3,17 @@ using TAC_COM.Audio.Utils;
 
 namespace TAC_COM.Models
 {
-    public class Profile()
+    public class Profile(string profileName, string fileIdentifier, Uri theme)
     {
-        public string? ProfileName;
-        public string? FileIdentifier;
+        public string ProfileName = profileName;
+        public string FileIdentifier = fileIdentifier;
+        public Uri Theme = theme;
         public IWaveSource? NoiseSource;
         public IWaveSource? OpenSFX;
         public IWaveSource? CloseSFX;
         private readonly FilePlayer FilePlayer = new();
         public ProfileSettings ProfileSettings = new();
-
+        
         public void LoadSources()
         {
             if (FileIdentifier != null)
@@ -27,7 +28,6 @@ namespace TAC_COM.Models
         {
             return ProfileName ?? string.Empty;
         }
-
     }
 
     public class ProfileSettings
@@ -43,36 +43,43 @@ namespace TAC_COM.Models
         {
             List<Profile> defaultProfiles = [];
 
-            defaultProfiles.Add(new Profile()
-            {
-                ProfileName = "GMS Type-4 Datalink",
-                FileIdentifier = "GMS",
-            });
-            defaultProfiles.Add(new Profile()
-            {
-                ProfileName = "IPS-N Itegrated Tactical Network",
-                FileIdentifier = "IPSN",
-            });
-            defaultProfiles.Add(new Profile()
-            {
-                ProfileName = "SSC Hamadryas Stealth Tranceiver",
-                FileIdentifier = "SSC",
-            });
-            defaultProfiles.Add(new Profile()
-            {
-                ProfileName = "HA Hardened Waveform Radio",
-                FileIdentifier = "HA",
-            });
-            defaultProfiles.Add(new Profile()
-            {
-                ProfileName = "HORUS [UNRECOGNISED DEVICE]",
-                FileIdentifier = "HORUS",
-                ProfileSettings = new ProfileSettings()
-                {
-                    PitchShiftFactor = 0.98f,
-                    ChorusEnabled = true,
-                }
-            });
+            defaultProfiles.Add(
+                new Profile(
+                    profileName: "GMS Type-4 Datalink",
+                    fileIdentifier: "GMS",
+                    theme: new Uri("pack://application:,,,/Themes/ThemeGMS.xaml", UriKind.Absolute)));
+
+            defaultProfiles.Add(
+                new Profile(
+                    profileName: "IPS-N Itegrated Tactical Network",
+                    fileIdentifier: "IPSN",
+                    theme: new Uri("pack://application:,,,/Themes/ThemeIPSN.xaml", UriKind.Absolute)));
+
+            defaultProfiles.Add(
+                new Profile(
+                    profileName: "SSC Hamadryas Stealth Tranceiver",
+                    fileIdentifier: "SSC",
+                    theme: new Uri("pack://application:,,,/Themes/ThemeSSC.xaml", UriKind.Absolute)));
+
+            defaultProfiles.Add(
+                new Profile(
+                    profileName: "HA Hardened Waveform Radio",
+                    fileIdentifier: "HA",
+                    theme: new Uri("pack://application:,,,/Themes/ThemeHA.xaml", UriKind.Absolute)));
+
+            defaultProfiles.Add(
+                new Profile(
+                    profileName: "HORUS [UNRECOGNISED DEVICE]",
+                    fileIdentifier: "HORUS",
+                    theme: new Uri("pack://application:,,,/Themes/ThemeHORUS.xaml", UriKind.Absolute))
+                { 
+                    ProfileSettings = new ProfileSettings()
+                    {
+                        PitchShiftFactor = 0.98f,
+                        ChorusEnabled = true,
+                    }
+                });
+
             return defaultProfiles;
         }
     }
