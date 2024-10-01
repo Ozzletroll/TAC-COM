a0cd5e8040d5fd73e19c9477ba4b08c92e2b03ae
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-10-01 11:45:55 +0100

    Add ThemeService

---
+++ Services/ThemeService.cs
@@ -0,0 +1,18 @@
+﻿using AdonisUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TAC_COM.Services
+{
+    public class ThemeService
+    {
+        public static void ChangeTheme(Uri currentTheme, Uri targetTheme)
+        {
+            ResourceLocator.SetColorScheme(Application.Current.Resources, currentTheme, targetTheme);
+        }
+    }
+}
