99bbc480156f9c3caba0ec961f840056f5536b91
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-10-03 16:34:09 +0100

    Add icons for each theme

--- Models/Profile.cs
+++ Models/Profile.cs
@@ -50,35 +50,35 @@ namespace TAC_COM.Models
                     profileName: "GMS Type-4 Datalink",
                     fileIdentifier: "GMS",
                     theme: new Uri("pack://application:,,,/Themes/ThemeGMS.xaml", UriKind.Absolute),
-                    icon: new BitmapImage(new Uri("pack://application:,,,/Static/Icons/icon.png"))));
+                    icon: new BitmapImage(new Uri("pack://application:,,,/Static/Icons/Icon-GMS.ico"))));
 
             defaultProfiles.Add(
                 new Profile(
                     profileName: "SSC Hamadryas Stealth Tranceiver",
                     fileIdentifier: "SSC",
                     theme: new Uri("pack://application:,,,/Themes/ThemeSSC.xaml", UriKind.Absolute),
-                    icon: new BitmapImage(new Uri("pack://application:,,,/Static/Icons/icon.png"))));
+                    icon: new BitmapImage(new Uri("pack://application:,,,/Static/Icons/Icon-GMS.ico"))));
 
             defaultProfiles.Add(
                 new Profile(
                     profileName: "IPS-N Integrated Tactical Network",
                     fileIdentifier: "IPSN",
                     theme: new Uri("pack://application:,,,/Themes/ThemeIPSN.xaml", UriKind.Absolute),
-                    icon: new BitmapImage(new Uri("pack://application:,,,/Static/Icons/icon.png"))));
+                    icon: new BitmapImage(new Uri("pack://application:,,,/Static/Icons/Icon-IPSN.ico"))));
 
             defaultProfiles.Add(
                 new Profile(
                     profileName: "HA Hardened Waveform Radio",
                     fileIdentifier: "HA",
                     theme: new Uri("pack://application:,,,/Themes/ThemeHA.xaml", UriKind.Absolute),
-                    icon: new BitmapImage(new Uri("pack://application:,,,/Static/Icons/icon.png"))));
+                    icon: new BitmapImage(new Uri("pack://application:,,,/Static/Icons/Icon-HA.ico"))));
 
             defaultProfiles.Add(
                 new Profile(
                     profileName: "HORUS [UNRECOGNISED DEVICE]",
                     fileIdentifier: "HORUS",
                     theme: new Uri("pack://application:,,,/Themes/ThemeHORUS.xaml", UriKind.Absolute),
-                    icon: new BitmapImage(new Uri("pack://application:,,,/Static/Icons/icon.png")))
+                    icon: new BitmapImage(new Uri("pack://application:,,,/Static/Icons/Icon-HORUS.ico")))
                     { 
                     ProfileSettings = new ProfileSettings()
                     {
