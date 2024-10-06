fdd3c9a8276ff2e089c23014e1c0ccf87d97807d
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-10-07 00:10:06 +0100

    Add NWaves VocoderEffect wrapper

---
+++ App/Audio/DSP/NWaves/VocoderEffectWrapper.cs
@@ -0,0 +1,80 @@
+﻿using CSCore;
+using NWaves.Effects;
+
+namespace App.Audio.DSP.NWaves
+{
+    internal class VocoderEffectWrapper(ISampleSource inputSource) : ISampleSource
+    {
+        readonly ISampleSource Source = inputSource;
+        private readonly PitchShiftVocoderEffect Vocoder = new(inputSource.WaveFormat.SampleRate, 1);
+
+        public float Wet
+        {
+            get => Vocoder.Wet;
+            set
+            {
+                Vocoder.Wet = value;
+            }
+        }
+
+        public float Dry
+        {
+            get => Vocoder.Dry;
+            set
+            {
+                Vocoder.Dry = value;
+            }
+        }
+
+        public float Shift
+        {
+            get => Vocoder.Shift;
+            set
+            {
+                Vocoder.Shift = value;
+            }
+        }
+
+        public int Read(float[] buffer, int offset, int count)
+        {
+            int samples = Source.Read(buffer, offset, count);
+
+            for (int i = offset; i < offset + samples; i++)
+            {
+                buffer[i] = Vocoder.Process(buffer[i]);
+            }
+            return samples;
+        }
+
+        public bool CanSeek
+        {
+            get { return Source.CanSeek; }
+        }
+
+        public WaveFormat WaveFormat
+        {
+            get { return Source.WaveFormat; }
+        }
+
+        public long Position
+        {
+            get
+            {
+                return Source.Position;
+            }
+            set
+            {
+                Source.Position = value;
+            }
+        }
+
+        public long Length
+        {
+            get { return Source.Length; }
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
