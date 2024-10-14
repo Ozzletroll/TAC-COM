b91622f53355de1a38c8f1fadbf69f7cdc44b7c2
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-10-14 15:22:15 +0100

    Add UriService

---
+++ App/Services/UriService.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Media.Imaging;
+
+namespace TAC_COM.Services
+{
+    public class UriService : IUriService
+    {
+        public Uri GetThemeUri(string themeName)
+        {
+            return new Uri($"pack://application:,,,/Themes/Theme{themeName}.xaml", UriKind.Absolute);
+        }
+
+        public BitmapImage GetIconUri(string iconName)
+        {
+            return new BitmapImage(new Uri($"pack://application:,,,/Static/Icons/Icon-{iconName}.ico"));
+        }
+    }
+
+    public interface IUriService
+    {
+        Uri GetThemeUri(string themeName);
+        BitmapImage GetIconUri(string IconName);
+    }
+}
