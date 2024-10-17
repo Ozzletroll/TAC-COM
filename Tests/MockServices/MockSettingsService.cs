0a93e985be3014beaebfba00d69d447b871eff4c
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-10-17 15:58:54 +0100

    Update MockSettingsService to handle settings

--- Tests/MockServices/MockSettingsService.cs
+++ Tests/MockServices/MockSettingsService.cs
@@ -8,7 +8,31 @@ namespace Tests.MockServices
         public AudioSettings AudioSettings { get; set; }
         public KeybindSettings KeybindSettings { get; set; }
 
-        public void UpdateAppConfig(string propertyName, object value) { }
+        public void UpdateAppConfig(string propertyName, object value) 
+        {
+            var property = AudioSettings.GetType().GetProperty(propertyName)
+                ?? KeybindSettings.GetType().GetProperty(propertyName);
+
+            // Update AppConfig
+            if (property != null)
+            {
+                if (property.PropertyType == typeof(string))
+                {
+                    property.SetValue(property.DeclaringType
+                        == typeof(AudioSettings) ? AudioSettings : KeybindSettings, value.ToString());
+                }
+                else if (property.PropertyType == typeof(float))
+                {
+                    property.SetValue(property.DeclaringType
+                        == typeof(AudioSettings) ? AudioSettings : KeybindSettings, (float)value);
+                }
+                else if (property.PropertyType == typeof(bool))
+                {
+                    property.SetValue(property.DeclaringType
+                        == typeof(AudioSettings) ? AudioSettings : KeybindSettings, (bool)value);
+                }
+            }
+        }
 
         public MockSettingsService()
         {
