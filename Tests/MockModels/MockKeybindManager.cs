ff89c6e794388de3d80c9c5514cd88e2fbcb73c0
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-10-22 16:29:03 +0100

    Add TestNewKeybindNameProperty

---
+++ Tests/MockModels/MockKeybindManager.cs
@@ -0,0 +1,31 @@
+﻿using Dapplo.Windows.Input.Keyboard;
+using System.ComponentModel;
+using TAC_COM.Models;
+using TAC_COM.Models.Interfaces;
+
+namespace Tests.MockModels
+{
+    internal class MockKeybindManager : IKeybindManager
+    {
+        public Keybind? NewPTTKeybind {  get; set; }
+        public bool PassthroughState { get; set; }
+        public Keybind? PTTKey { get; set; }
+        public bool ToggleState { get; set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void InitialisePTTKeySubscription() { }
+
+        public void InitialiseUserKeybindSubscription() { }
+
+        public void LoadKeybindSettings() { }
+
+        public void TogglePTT(KeyboardHookEventArgs args) { }
+
+        public void TogglePTTKeybind(bool state) { }
+
+        public void ToggleUserKeybind(bool state) { }
+
+        public void UpdateKeybind() { }
+    }
+}
---
+++ Tests/Tests/ViewModelTests/KeybindWindowViewModelTests.cs
@@ -0,0 +1,31 @@
+﻿using TAC_COM.Models.Interfaces;
+using TAC_COM.Services.Interfaces;
+using TAC_COM.ViewModels;
+using Tests.MockModels;
+using Tests.MockServices;
+
+namespace Tests.ViewModelTests
+{
+    [TestClass]
+    public partial class KeybindWindowViewModelTests
+    {
+        public ISettingsService settingsService;
+        public IKeybindManager keybindManager;
+        public KeybindWindowViewModel testViewModel;
+
+        public KeybindWindowViewModelTests()
+        {
+            settingsService = new MockSettingsService();
+            keybindManager = new MockKeybindManager();
+            testViewModel = new KeybindWindowViewModel(keybindManager);
+        }
+
+        [TestMethod]
+        public void TestNewKeybindNameProperty()
+        {
+            Utils.TestPropertyChange(testViewModel, nameof(testViewModel.NewKeybindName), "Shift + V");
+            Assert.IsTrue(testViewModel.NewKeybindName == "[ Shift + V ]");
+        }
+
+    }
+}
