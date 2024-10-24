288e105df6909e27793b3ba774e95097e73b0d21
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-10-24 14:45:15 +0100

    Refactor ViewModelBase to use NotifyProperty util

--- App/ViewModels/ViewModelBase.cs
+++ App/ViewModels/ViewModelBase.cs
@@ -1,14 +1,6 @@
-﻿using System.ComponentModel;
+﻿using TAC_COM.Utilities;
 
 namespace TAC_COM.ViewModels
 {
-    public class ViewModelBase : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        protected void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-    }
+    public class ViewModelBase : NotifyProperty { }
 }
