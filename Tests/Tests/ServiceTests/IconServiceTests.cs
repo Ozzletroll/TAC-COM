576650887e28ed43de6968ee885956929eb97326
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-10-30 14:49:35 +0000

    Add TestSetLiveIcon

---
+++ Tests/Tests/ServiceTests/IconServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using TAC_COM.Services;
+
+namespace Tests.ServiceTests
+{
+
+    [TestClass]
+    public partial class IconServiceTests
+    {
+        public IconService testIconService;
+
+        public IconServiceTests()
+        {
+            testIconService = new IconService();
+        }
+
+        [TestMethod]
+        public void TestSetLiveIcon()
+        {
+            IconChangeEventArgs? raisedEventArgs = null;
+
+            testIconService.ChangeSystemTrayIcon += (sender, e) =>
+            {
+                raisedEventArgs = e as IconChangeEventArgs;
+            };
+
+            testIconService.SetLiveIcon();
+
+            Assert.IsNotNull(raisedEventArgs);
+            Assert.AreEqual("./Static/Icons/live.ico", raisedEventArgs?.IconPath);
+            Assert.AreEqual("TAC/COM Live", raisedEventArgs?.Tooltip);
+        }
+    }
+}
