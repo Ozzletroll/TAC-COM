ea626b150d14c4f78062c1c9e2290a28b35fc45f
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-10-31 15:20:27 +0000

    Add TestUpdateAppConfig_AudioSettings

--- Tests/Tests/ServiceTests/SettingsServiceTests.cs
+++ Tests/Tests/ServiceTests/SettingsServiceTests.cs
@@ -1,4 +1,6 @@
-﻿using TAC_COM.Services;
+﻿using System.Windows.Input;
+using TAC_COM.Services;
+using TAC_COM.Services.Interfaces;
 
 namespace Tests.ServiceTests
 {
@@ -18,6 +20,30 @@ namespace Tests.ServiceTests
             Assert.IsNotNull(testSettingsService.AppConfig); 
             Assert.IsNotNull(testSettingsService.AudioSettings); 
             Assert.IsNotNull(testSettingsService.KeybindSettings);
+        }
+
+        [TestMethod]
+        public void TestUpdateAppConfig_AudioSettings()
+        {
+            var testStringProperty = "InputDevice";
+            var testStringValue = "Test Input Device";
+
+            testSettingsService.UpdateAppConfig(testStringProperty, testStringValue);
+
+            var stringPropertyInfo = testSettingsService.AudioSettings.GetType().GetProperty(testStringProperty);
+            var stringPropertyValue = stringPropertyInfo?.GetValue(testSettingsService.AudioSettings);
+
+            Assert.AreEqual(testStringValue, stringPropertyValue);
+
+            var testFloatProperty = "NoiseGateThreshold";
+            var testFloatValue = -65f;
+
+            testSettingsService.UpdateAppConfig(testFloatProperty, testFloatValue);
+
+            var floatPropertyInfo = testSettingsService.AudioSettings.GetType().GetProperty(testFloatProperty);
+            var floatPropertyValue = floatPropertyInfo?.GetValue(testSettingsService.AudioSettings);
+
+            Assert.AreEqual(testFloatValue, floatPropertyValue);
         }
     }
 }
