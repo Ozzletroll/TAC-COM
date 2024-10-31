f5e25ddfe5460a6885b5e41ee039e8dd3b32da3c
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-10-31 14:49:46 +0000

    Add TestConstructor

---
+++ Tests/Tests/ServiceTests/SettingsServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using TAC_COM.Services;
+
+namespace Tests.ServiceTests
+{
+    [TestClass]
+    public partial class SettingsServiceTests
+    {
+        private readonly SettingsService testSettingsService;
+
+        public SettingsServiceTests() 
+        { 
+            testSettingsService = new SettingsService();
+        }
+
+        [TestMethod]
+        public void TestConstructor()
+        {
+            Assert.IsNotNull(testSettingsService.AppConfig); 
+            Assert.IsNotNull(testSettingsService.AudioSettings); 
+            Assert.IsNotNull(testSettingsService.KeybindSettings);
+        }
+    }
+}
