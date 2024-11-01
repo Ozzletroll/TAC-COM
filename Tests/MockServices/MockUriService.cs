14bd7174509f58abe3b48f2b3b7d894ba6c3158f
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-11-01 23:55:45 +0000

    Add TestChangeTheme

--- Tests/MockServices/MockUriService.cs
+++ Tests/MockServices/MockUriService.cs
@@ -5,10 +5,18 @@ namespace Tests.MockServices
 {
     public class MockUriService : IUriService
     {
-        public Uri GetThemeUri(string themeName) => new("http://mock.uri/" + themeName);
+        public Uri GetThemeUri(string themeName)
+        {
+            string relativePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "MockResources", "MockTheme.xaml");
+            return new Uri(relativePath, UriKind.Absolute); 
+        }
 
         public BitmapImage GetIconUri(string iconName) => new(new Uri("http://mock.uri/" + iconName));
 
-        public Uri GetResourcesUri() => new("http://mock.uri/");
+        public Uri GetResourcesUri()
+        {
+            string relativePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "MockResources", "MockResources.xaml");
+            return new Uri(relativePath, UriKind.Absolute);
+        }
     }
 }
---
+++ Tests/Tests/ServiceTests/ThemeServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using TAC_COM.Services;
+using Tests.MockServices;
+
+namespace Tests.ServiceTests
+{
+    [TestClass]
+    public partial class ThemeServiceTests
+    {
+        private readonly ThemeService themeService;
+        private readonly MockUriService mockUriService = new();
+
+        public ThemeServiceTests() 
+        {
+            themeService = new ThemeService(mockUriService);
+        }
+
+        [TestMethod]
+        public void TestChangeTheme()
+        {
+            _ = new Application();
+
+            var newThemeUri = mockUriService.GetThemeUri("MockThemeName"); 
+            themeService.ChangeTheme(newThemeUri); 
+
+            var rootResourceDictionary = Application.Current.Resources; 
+            var currentThemeUri = rootResourceDictionary.MergedDictionaries.FirstOrDefault()?.Source; Assert.AreEqual(newThemeUri, currentThemeUri);
+
+            Assert.AreEqual(newThemeUri, currentThemeUri);
+        }
+    }
+}
