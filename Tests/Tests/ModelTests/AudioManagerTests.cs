57dd9950f9c407ce8c121a460e73fd2183429b98
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-11-07 17:57:40 +0000

    Add TestBypassStateProperty

--- Tests/Tests/ModelTests/AudioManagerTests.cs
+++ Tests/Tests/ModelTests/AudioManagerTests.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections.ObjectModel;
+﻿using CSCore;
+using CSCore.CoreAudioAPI;
+using CSCore.SoundIn;
+using CSCore.Streams;
+using Moq;
+using System.Collections.ObjectModel;
 using System.Windows.Media.Imaging;
 using TAC_COM.Models;
 using TAC_COM.Models.Interfaces;
@@ -57,6 +62,27 @@ namespace Tests.ModelTests
             var newPropertyValue = true;
             Utils.TestPropertyChange(audioManager, nameof(audioManager.State), newPropertyValue);
             Assert.AreEqual(audioManager.State, newPropertyValue);
+        }
+
+        [TestMethod]
+        public void TestBypassStateProperty()
+        {
+            var mockAudioProcessor = new Mock<AudioProcessor>();
+
+            var mockWetNoiseMix = new VolumeSource(new Mock<ISampleSource>().Object);
+            var mockDryMix = new VolumeSource(new Mock<ISampleSource>().Object);
+
+            mockAudioProcessor.Object.HasInitialised = true;
+            mockAudioProcessor.Object.WetNoiseMixLevel = mockWetNoiseMix;
+            mockAudioProcessor.Object.DryMixLevel = mockDryMix;
+
+            audioManager.AudioProcessor = mockAudioProcessor.Object;
+
+            var newPropertyValue = true;
+            Utils.TestPropertyChange(audioManager, nameof(audioManager.BypassState), newPropertyValue);
+            Assert.AreEqual(audioManager.BypassState, newPropertyValue);
+            Assert.IsTrue(mockAudioProcessor.Object.WetNoiseMixLevel.Volume == 1);
+            Assert.IsTrue(mockAudioProcessor.Object.DryMixLevel.Volume == 0);
         }
     }
 }
