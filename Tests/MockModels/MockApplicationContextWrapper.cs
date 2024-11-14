c95202a65654556d519565cd80d245bb5ebc9164
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-11-14 20:06:13 +0000

    Add MockApplicationContextWrapper

---
+++ Tests/MockModels/MockApplicationContextWrapper.cs
@@ -0,0 +1,20 @@
+﻿using App.Models.Interfaces;
+using System.Windows;
+
+namespace Tests.MockModels
+{
+    public class MockApplicationContextWrapper(Window mainWindow) : IApplicationContextWrapper
+    {
+        public Window MainWindow { get; set; } = mainWindow;
+
+        private ResourceDictionary resources = [];
+        public ResourceDictionary Resources 
+        { 
+            get => resources; 
+            set
+            {
+                resources = value;
+            } 
+        }
+    }
+}
