07fdf1eba829e5ff58b3f941c2e04922b0eb0b47
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-11-21 15:46:45 +0000

    Add WasapiOutWrapper and interface

---
+++ TAC_COM/Models/Interfaces/IWasapiOutWrapper.cs
@@ -0,0 +1,19 @@
+﻿using CSCore;
+using CSCore.CoreAudioAPI;
+using CSCore.SoundIn;
+using CSCore.SoundOut;
+
+namespace TAC_COM.Models.Interfaces
+{
+    public interface IWasapiOutWrapper
+    {
+        MMDevice Device { get; set; }
+
+        event EventHandler<PlaybackStoppedEventArgs> Stopped;
+
+        void Dispose();
+        void Initialize(IWaveSource? source);
+        void Play();
+        void Stop();
+    }
+}
