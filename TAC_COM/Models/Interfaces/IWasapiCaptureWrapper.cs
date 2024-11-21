ed4a272e52edc5ed91647f6da3d4a7aa1785d097
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-11-21 15:46:13 +0000

    Add WasapiCaptureWrapper and interface

---
+++ TAC_COM/Models/Interfaces/IWasapiCaptureWrapper.cs
@@ -0,0 +1,19 @@
+﻿using CSCore.CoreAudioAPI;
+using CSCore.SoundIn;
+
+namespace TAC_COM.Models.Interfaces
+{
+    public interface IWasapiCaptureWrapper
+    {
+        public WasapiCapture WasapiCapture { get; }
+        MMDevice Device { get; set; }
+
+        event EventHandler<DataAvailableEventArgs> DataAvailable;
+        event EventHandler<RecordingStoppedEventArgs> Stopped;
+
+        void Dispose();
+        void Initialize();
+        void Start();
+        void Stop();
+    }
+}
