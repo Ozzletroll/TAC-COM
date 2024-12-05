b9c4909cb8bd64b2a8d167f596576926c13a664b
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-12-05 11:55:22 +0000

    Add TestKeyCode

---
+++ Tests/UnitTests/ModelTests/KeybindTests.cs
@@ -0,0 +1,30 @@
+﻿using Dapplo.Windows.Input.Enums;
+using TAC_COM.Models;
+
+namespace Tests.UnitTests.ModelTests
+{
+    [TestClass]
+    public class KeybindTests
+    {
+        private readonly Keybind testKeybind;
+
+        public KeybindTests()
+        {
+            testKeybind = new Keybind(
+                keyCode: VirtualKeyCode.KeyV,
+                shift: false,
+                ctrl: false,
+                alt: false,
+                isModifier: false,
+                passthrough: false);
+        }
+
+        [TestMethod]
+        public void TestKeyCode()
+        {
+            var newPropertyValue = VirtualKeyCode.KeyF;
+            testKeybind.KeyCode = newPropertyValue;
+            Assert.AreEqual(testKeybind.KeyCode, newPropertyValue);
+        }
+    }
+}
