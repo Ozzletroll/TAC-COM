ba3c8ae4304db2ae7fcacae58de1f38596b2dba4
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-12-09 13:28:23 +0000

    Add MockSampleSource

---
+++ Tests/MockModels/MockSampleSource.cs
@@ -0,0 +1,25 @@
+﻿using CSCore;
+
+namespace Tests.MockModels
+{
+    public class MockSampleSource : ISampleSource
+    {
+        public bool CanSeek => throw new NotImplementedException();
+
+        public WaveFormat WaveFormat => throw new NotImplementedException();
+
+        public long Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public long Length => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Read(float[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
