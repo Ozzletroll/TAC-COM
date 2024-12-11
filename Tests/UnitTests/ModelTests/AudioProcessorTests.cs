1e8fc3f0cb8b7a54faf2ce87e4a47dd6aa164a50
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-12-11 11:56:07 +0000

    Add TestHasInitialisedProperty

---
+++ Tests/UnitTests/ModelTests/AudioProcessorTests.cs
@@ -0,0 +1,24 @@
+﻿using TAC_COM.Models;
+using TAC_COM.Models.Interfaces;
+
+namespace Tests.UnitTests.ModelTests
+{
+    [TestClass]
+    public class AudioProcessorTests
+    {
+        private readonly AudioProcessor audioProcessor;
+
+        public AudioProcessorTests()
+        {
+            audioProcessor = new AudioProcessor();
+        }
+
+        [TestMethod]
+        public void TestHasInitialisedProperty()
+        {
+            var newPropertyValue = true;
+            audioProcessor.HasInitialised = newPropertyValue;
+            Assert.AreEqual(audioProcessor.HasInitialised, newPropertyValue);
+        }
+    }
+}
