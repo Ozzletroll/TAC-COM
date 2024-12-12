0d52d47e5d341adf49b6359a58eb790ac042414c
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-12-12 15:30:23 +0000

    Add MockWaveSource

---
+++ Tests/MockModels/MockWaveSource.cs
@@ -0,0 +1,22 @@
+﻿using CSCore;
+
+namespace Tests.MockModels
+{
+    internal class MockWaveSource : IWaveSource
+    {
+        public bool CanSeek => throw new NotImplementedException();
+
+        public WaveFormat WaveFormat => new();
+
+        public long Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public long Length => throw new NotImplementedException();
+
+        public void Dispose() { }
+
+        public int Read(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
