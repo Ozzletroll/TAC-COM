fd9cfc1bac00945192c4a50f779358a671ab9a6e
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-12-12 16:52:42 +0000

    Add TestGetInputDevices

---
+++ Tests/UnitTests/ServiceTests/MMDeviceEnumeratorTests.cs
@@ -0,0 +1,26 @@
+﻿using TAC_COM.Models.Interfaces;
+using TAC_COM.Services;
+
+namespace Tests.UnitTests.ServiceTests
+{
+    [TestClass]
+    public class MMDeviceEnumeratorTests
+    {
+        private readonly MMDeviceEnumeratorService enumeratorService = new();
+
+        [TestMethod]
+        public void TestGetInputDevices()
+        {
+            var inputDevices = enumeratorService.GetInputDevices();
+
+            Assert.IsNotNull(inputDevices);
+            Assert.IsTrue(inputDevices.Any());
+
+            foreach (var deviceWrapper in inputDevices)
+            {
+                Assert.IsInstanceOfType(deviceWrapper, typeof(IMMDeviceWrapper));
+                Assert.IsNotNull(deviceWrapper.Device);
+            }
+        }
+    }
+}
