a3c2f867303baf0cd790ba5f93db0d3085836437
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-12-13 15:04:45 +0000

    Add TestCreateWaspiOut

---
+++ Tests/UnitTests/ServiceTests/WasapiServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using TAC_COM.Models;
+using TAC_COM.Services;
+
+namespace Tests.UnitTests.ServiceTests
+{
+    [TestClass]
+    public class WasapiServiceTests
+    {
+        private readonly WasapiService wasapiService = new();
+
+        [TestMethod]
+        public void TestCreateWaspiCapture()
+        {
+            var output = wasapiService.CreateWasapiCapture();
+
+            Assert.IsNotNull(output);
+            Assert.IsInstanceOfType(output, typeof(WasapiCaptureWrapper));
+        }
+
+        [TestMethod]
+        public void TestCreateWaspiOut()
+        {
+            var output = wasapiService.CreateWasapiOut();
+
+            Assert.IsNotNull(output);
+            Assert.IsInstanceOfType(output, typeof(WasapiOutWrapper));
+        }
+    }
+}
