c984d473f9a0f1a6c239e3aadb6488973f2451a8
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-12-16 16:06:39 +0000

    Update EffectsReferenceTests to test all wrappers

--- Tests/UnitTests/ModelTests/EffectsReferenceTests.cs
+++ Tests/UnitTests/ModelTests/EffectsReferenceTests.cs
@@ -1,6 +1,7 @@
-﻿using System.Reflection;
-using CSCore;
-using TAC_COM.Audio.DSP.NWaves;
+﻿using CSCore;
+using NWaves.Effects;
+using NWaves.Operations;
+using TAC_COM.Audio.DSP.EffectReferenceWrappers;
 using TAC_COM.Models;
 using Tests.MockModels;
 
@@ -9,29 +10,176 @@ namespace Tests.UnitTests.ModelTests
     [TestClass]
     public class EffectsReferenceTests
     {
+        private static List<EffectReference> GetAllEffectReferenceWrappers()
+        {
+            List<EffectReference> allEffectsReferences = [];
+
+            allEffectsReferences.Add(
+                new(typeof(BitCrusherWrapper))
+                {
+                    Parameters = new Dictionary<string, object>
+                    {
+                        { "Wet", 0.5f },
+                        { "Dry", 0.5f },
+                        { "BitDepth", 5 }
+                    }
+                }
+            );
+
+            allEffectsReferences.Add(
+                new(typeof(ChorusWrapper))
+                {
+                    Parameters = new Dictionary<string, object>
+                    {
+                        { "Wet", 0.1f },
+                        { "Dry", 0.9f },
+                        { "LFOFrequencies", new float[] { 800, 3000, 6000 } },
+                        { "Widths", new float[] {0.3f, 0.8f, 1.5f } },
+                    }
+                }
+            );
+
+            allEffectsReferences.Add(
+                new(typeof(DistortionWrapper))
+                {
+                    Parameters = new Dictionary<string, object>
+                    {
+                        { "Mode", DistortionMode.HalfWaveRectify },
+                        { "Wet", 0.1f },
+                        { "Dry", 0.9f },
+                        { "InputGainDB", 25 },
+                        { "OutputGainDB", 15 },
+                    }
+                }
+            );
+
+            allEffectsReferences.Add(
+                new(typeof(DynamicsProcessorWrapper))
+                {
+                    Parameters = new Dictionary<string, object>
+                    {
+                       { "Mode", DynamicsMode.Compressor },
+                       { "MinAmplitude", -120 },
+                       { "Threshold", -20 },
+                       { "Ratio", 100 },
+                       { "Attack", 30 },
+                       { "Release", 300 },
+                       { "MakeupGain", 10 },
+                    }
+                }
+            );
+
+            allEffectsReferences.Add(
+                new(typeof(EchoWrapper))
+                {
+                    Parameters = new Dictionary<string, object>
+                    {
+                        { "Wet", 0.1f },
+                        { "Dry", 0.9f },
+                        { "Delay", 0.003f }
+                    }
+                }
+            );
+
+            allEffectsReferences.Add(
+                new(typeof(FlangerWrapper))
+                {
+                    Parameters = new Dictionary<string, object>
+                    {
+                        { "Wet", 0.1f },
+                        { "Dry", 0.9f },
+                        { "LfoFrequency", 50f },
+                        { "Width", 0.05f },
+                        { "Depth", 0.3f },
+                        { "Feedback", 0.5f }
+                    }
+                }
+            );
+
+            allEffectsReferences.Add(
+                new(typeof(RobotEffectWrapper))
+                {
+                    Parameters = new Dictionary<string, object>
+                    {
+                        { "Wet", 0.1f },
+                        { "Dry", 0.9f },
+                    }
+                }
+            );
+
+            allEffectsReferences.Add(
+                new(typeof(TubeDistortionWrapper))
+                {
+                    Parameters = new Dictionary<string, object>
+                    {
+                        { "Wet", 0.1f },
+                        { "Dry", 0.9f },
+                        { "InputGainDB", 25 },
+                        { "OutputGainDB", 13 },
+                        { "Q", -0.2f },
+                        { "Distortion", 25 },
+                    }
+                }
+            );
+
+            allEffectsReferences.Add(
+                new(typeof(VocoderEffectWrapper))
+                {
+                    Parameters = new Dictionary<string, object>
+                    {
+                        { "Wet", 0.1f },
+                        { "Dry", 0.9f },
+                        { "Shift", 0.85f }
+                    }
+                }
+            );
+
+            allEffectsReferences.Add(
+                new(typeof(WhisperWrapper))
+                {
+                    Parameters = new Dictionary<string, object>
+                    {
+                        { "Wet", 0.1f },
+                        { "Dry", 0.9f },
+                    }
+                }
+            );
+
+            return allEffectsReferences;
+        }
+
         [TestMethod]
         public void TestCreateInstance()
         {
-            ISampleSource testSampleSource = new MockSampleSource();
 
-            EffectReference testEffectReference = new(typeof(BitCrusherWrapper))
+            var allEffectReferences = GetAllEffectReferenceWrappers();
+            Assert.IsNotNull(allEffectReferences);
+
+            foreach (EffectReference effectReference in allEffectReferences) 
             {
-                Parameters = new Dictionary<string, object>
-                {
-                    { "Wet", 0.5f },
-                    { "Dry", 0.5f },
-                    { "BitDepth", 5 }
+                ISampleSource testSampleSource = new MockSampleSource();
+
+                var outputSampleSource = testSampleSource.AppendSource(x => effectReference.CreateInstance(x));
+
+                Assert.IsNotNull(outputSampleSource, $"EffectReference '{effectReference.EffectType}' failed to instantiate.");
+                Assert.IsInstanceOfType(outputSampleSource, typeof(ISampleSource));
+                Assert.IsNotNull(effectReference.Parameters, "EffectReference was not created with parameters dictionary.");
+
+                foreach (var (parameterName, parameterValue) in effectReference.Parameters) 
+                { 
+                    // Find the corresponding property in the outputSampleSource
+                    var outputProperty = outputSampleSource.GetType().GetProperty(parameterName); 
+                    if (outputProperty != null) 
+                    { 
+                        object? outputValue = outputProperty.GetValue(outputSampleSource);
+                        Assert.IsNotNull(outputValue);
+                    } 
+                    else 
+                    { 
+                        Assert.Fail($"Property '{parameterName}' not found on outputSampleSource.");
+                    }
                 }
-            };
-
-            var outputSampleSource = testSampleSource.AppendSource(x => testEffectReference.CreateInstance(x));
-
-            FieldInfo? effectField = typeof(BitCrusherWrapper).GetField("bitCrusherEffect", BindingFlags.NonPublic | BindingFlags.Instance);
-            var effectValue = effectField?.GetValue(outputSampleSource);
-
-            Assert.IsNotNull(outputSampleSource);
-            Assert.IsInstanceOfType(outputSampleSource, typeof(ISampleSource));
-            Assert.IsNotNull(effectValue);
+            }
         }
     }
 }
