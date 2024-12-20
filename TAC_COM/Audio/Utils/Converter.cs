f6e0a124a72ea14136589a96381740143933d647
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-12-20 14:30:28 +0000

    Add XML documentation comments

--- TAC_COM/Audio/Utils/Converter.cs
+++ TAC_COM/Audio/Utils/Converter.cs
@@ -1,13 +1,29 @@
 ﻿
 namespace TAC_COM.Audio.Utils
 {
-    internal class LinearDBConverter
+    /// <summary>
+    /// Class to provide basic linear-to-decibel
+    /// and decibel-to-linear value converstions.
+    /// </summary>
+    public class LinearDBConverter
     {
+        /// <summary>
+        /// Convert a given linear gain value to a
+        /// decibel value.
+        /// </summary>
+        /// <param name="value"> The linear gain value to convert.</param>
+        /// <returns> The converted value in decibels.</returns>
         public static float LinearToDecibel(float value)
         {
             return 20 * (float)Math.Log10(value);
         }
 
+        /// <summary>
+        /// Convert a given decibel gain value to a
+        /// linear value.
+        /// </summary>
+        /// <param name="value"> The decibel value to convert.</param>
+        /// <returns> The converted value as a linear expression.</returns>
         public static float DecibelToLinear(float value)
         {
             return (float)Math.Pow(10, value / 20.0);
