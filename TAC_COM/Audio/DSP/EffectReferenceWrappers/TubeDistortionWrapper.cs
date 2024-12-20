02e257b04dc24ff04b4cc8fb6a85363ed292d9be
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-12-20 12:28:59 +0000

    Add XML documentation comments

--- TAC_COM/Audio/DSP/EffectReferenceWrappers/TubeDistortionWrapper.cs
+++ TAC_COM/Audio/DSP/EffectReferenceWrappers/TubeDistortionWrapper.cs
@@ -8,6 +8,11 @@ namespace TAC_COM.Audio.DSP.EffectReferenceWrappers
         private readonly ISampleSource source = inputSource;
         private readonly TubeDistortionEffect tubeDistortion = new();
 
+        /// <summary>
+        /// Gets or sets the value of the "wet" processed signal 
+        /// level of the effect.
+        /// Takes a value between 0 and 1.
+        /// </summary>
         public float Wet
         {
             get => tubeDistortion.Wet;
@@ -17,6 +22,11 @@ namespace TAC_COM.Audio.DSP.EffectReferenceWrappers
             }
         }
 
+        /// <summary>
+        /// Gets or sets the value of the "dry" unprocessed signal 
+        /// level of the effect.
+        /// Takes a value between 0 and 1.
+        /// </summary>
         public float Dry
         {
             get => tubeDistortion.Dry;
@@ -26,6 +36,9 @@ namespace TAC_COM.Audio.DSP.EffectReferenceWrappers
             }
         }
 
+        /// <summary>
+        /// Gets or sets the input gain in decibels.
+        /// </summary>
         public float InputGainDB
         {
             get => tubeDistortion.InputGain;
@@ -35,6 +48,9 @@ namespace TAC_COM.Audio.DSP.EffectReferenceWrappers
             }
         }
 
+        /// <summary>
+        /// Gets or sets the output gain in decibels.
+        /// </summary>
         public float OutputGainDB
         {
             get => tubeDistortion.OutputGain;
@@ -44,6 +60,11 @@ namespace TAC_COM.Audio.DSP.EffectReferenceWrappers
             }
         }
 
+        /// <summary>
+        /// Gets or sets Q factor (Work point). Controls the linearity of 
+        /// the transfer function for low input levels. 
+        /// More negative values result in a more linear function.
+        /// </summary>
         public float Q
         {
             get => tubeDistortion.Q;
@@ -53,6 +74,10 @@ namespace TAC_COM.Audio.DSP.EffectReferenceWrappers
             }
         }
 
+        /// <summary>
+        /// Gets or sets the character of the distortion effect.
+        /// Higher numbers equal harder distortion.
+        /// </summary>
         public float Distortion
         {
             get => tubeDistortion.Dist;
@@ -62,6 +87,14 @@ namespace TAC_COM.Audio.DSP.EffectReferenceWrappers
             }
         }
 
+        /// <summary>
+        /// Implementation of the <see cref="ISampleSource"/> Read method,
+        /// in which the effect is applied to the sample buffer.
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="offset"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
         public int Read(float[] buffer, int offset, int count)
         {
             int samples = source.Read(buffer, offset, count);
@@ -72,16 +105,28 @@ namespace TAC_COM.Audio.DSP.EffectReferenceWrappers
             return samples;
         }
 
+        /// <summary>
+        /// Implementation of the <see cref="ISampleSource"/> CanSeek
+        /// property.
+        /// </summary>
         public bool CanSeek
         {
             get { return source.CanSeek; }
         }
 
+        /// <summary>
+        /// Implementation of the <see cref="ISampleSource"/> WaveFormat
+        /// property.
+        /// </summary>
         public WaveFormat WaveFormat
         {
             get { return source.WaveFormat; }
         }
 
+        /// <summary>
+        /// Implementation of the <see cref="ISampleSource"/> Position
+        /// property.
+        /// </summary>
         public long Position
         {
             get
@@ -94,11 +139,19 @@ namespace TAC_COM.Audio.DSP.EffectReferenceWrappers
             }
         }
 
+        /// <summary>
+        /// Implementation of the <see cref="ISampleSource"/> Length
+        /// property.
+        /// </summary>
         public long Length
         {
             get { return source.Length; }
         }
 
+        /// <summary>
+        /// Implementation of the <see cref="ISampleSource"/> Dispose
+        /// method.
+        /// </summary>
         public void Dispose()
         {
             GC.SuppressFinalize(this);
