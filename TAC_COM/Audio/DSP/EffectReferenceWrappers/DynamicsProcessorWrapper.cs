1063e9d4c6b086dedf6f1f0c14bd05b1d4f8d6a6
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-12-20 12:09:57 +0000

    Add XML documentation comments

--- TAC_COM/Audio/DSP/EffectReferenceWrappers/DynamicsProcessorWrapper.cs
+++ TAC_COM/Audio/DSP/EffectReferenceWrappers/DynamicsProcessorWrapper.cs
@@ -3,12 +3,24 @@ using NWaves.Operations;
 
 namespace TAC_COM.Audio.DSP.EffectReferenceWrappers
 {
+    /// <summary>
+    /// Wrapper class to apply an <see cref="ChorusEffect"/> to a given
+    /// <see cref="ISampleSource"/>.
+    /// </summary>
+    /// <remarks>
+    /// Can be used as a <see cref="Models.EffectReference"/>.
+    /// </remarks>
+    /// <param name="inputSource">The <see cref="ISampleSource"/> to which the effect is to be applied.</param>
     public class DynamicsProcessorWrapper(ISampleSource inputSource) : ISampleSource
     {
         private readonly ISampleSource source = inputSource;
         private DynamicsProcessor processor = new(DynamicsMode.Compressor, inputSource.WaveFormat.SampleRate, 0, 1, -120);
 
         private DynamicsMode mode;
+
+        /// <summary>
+        /// Gets or sets the dynamics mode (compressor/expander/limiter/gate).
+        /// </summary>
         public DynamicsMode Mode
         {
             get => mode;
@@ -20,6 +32,10 @@ namespace TAC_COM.Audio.DSP.EffectReferenceWrappers
         }
 
         private float minAmplitude = -120;
+
+        /// <summary>
+        /// Gets or sets the minimum decibel amplitude.
+        /// </summary>
         public float MinAmplitude
         {
             get => minAmplitude;
@@ -30,6 +46,10 @@ namespace TAC_COM.Audio.DSP.EffectReferenceWrappers
             }
         }
 
+        /// <summary>
+        /// Gets or sets the threshold level in decibels
+        /// at which attenuation begins.
+        /// </summary>
         public float Threshold
         {
             get => processor.Threshold;
@@ -39,6 +59,9 @@ namespace TAC_COM.Audio.DSP.EffectReferenceWrappers
             }
         }
 
+        /// <summary>
+        /// Gets or sets the ratio of the dynamics processor.
+        /// </summary>
         public float Ratio
         {
             get => processor.Ratio;
@@ -48,6 +71,9 @@ namespace TAC_COM.Audio.DSP.EffectReferenceWrappers
             }
         }
 
+        /// <summary>
+        /// Gets or sets the attack in milliseconds.
+        /// </summary>
         public float Attack
         {
             get => processor.Attack;
@@ -57,6 +83,9 @@ namespace TAC_COM.Audio.DSP.EffectReferenceWrappers
             }
         }
 
+        /// <summary>
+        /// Gets or sets teh release in milliseconds.
+        /// </summary>
         public float Release
         {
             get => processor.Release;
@@ -66,6 +95,9 @@ namespace TAC_COM.Audio.DSP.EffectReferenceWrappers
             }
         }
 
+        /// <summary>
+        /// Gets or sets the overall makeup gain in decibels.
+        /// </summary>
         public float MakeupGain
         {
             get => processor.MakeupGain;
@@ -75,6 +107,14 @@ namespace TAC_COM.Audio.DSP.EffectReferenceWrappers
             }
         }
 
+        /// <summary>
+        /// Implementation of the <see cref="ISampleSource"/> Read method,
+        /// in which the effect is applied to the sample buffer.
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="offset"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
         public int Read(float[] buffer, int offset, int count)
         {
             int samples = source.Read(buffer, offset, count);
@@ -85,16 +125,28 @@ namespace TAC_COM.Audio.DSP.EffectReferenceWrappers
             return samples;
         }
 
+        /// <summary>
+        /// Implementation of the <see cref="ISampleSource"/> CanSeek
+        /// property.
+        /// </summary>
         public bool CanSeek
         {
             get { return source.CanSeek; }
         }
 
+        /// <summary>
+        /// Implementation of the <see cref="ISampleSource"/> WaveFormat
+        /// property.
+        /// </summary>
         public WaveFormat WaveFormat
         {
             get { return source.WaveFormat; }
         }
 
+        /// <summary>
+        /// Implementation of the <see cref="ISampleSource"/> Position
+        /// property.
+        /// </summary>
         public long Position
         {
             get
@@ -107,11 +159,19 @@ namespace TAC_COM.Audio.DSP.EffectReferenceWrappers
             }
         }
 
+        /// <summary>
+        /// Implementation of the <see cref="ISampleSource"/> Length
+        /// property.
+        /// </summary>
         public long Length
         {
             get { return source.Length; }
         }
 
+        /// <summary>
+        /// Implementation of the <see cref="ISampleSource"/> Dispose
+        /// method.
+        /// </summary>
         public void Dispose()
         {
             GC.SuppressFinalize(this);
