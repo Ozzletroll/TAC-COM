906da1a3488afd3618a98f3618acdd8f10a67213
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-12-20 17:52:42 +0000

    Add xml documentation comments

--- TAC_COM/Models/MMDeviceWrapper.cs
+++ TAC_COM/Models/MMDeviceWrapper.cs
@@ -3,12 +3,31 @@ using TAC_COM.Models.Interfaces;
 
 namespace TAC_COM.Models
 {
+    /// <summary>
+    /// Wrapper class for <see cref="MMDevice"/>, to
+    /// faciliate easier testing.
+    /// </summary>
+    /// <param name="device"> The <see cref="MMDevice"/> to
+    /// be used.</param>
     public class MMDeviceWrapper(MMDevice device) : IMMDeviceWrapper
     {
+        /// <summary>
+        /// Gets or sets the wrapped <see cref="MMDevice"/>.
+        /// </summary>
         public MMDevice Device { get; set; } = device;
 
+        /// <summary>
+        /// Gets the string FriendlyName of the wrapped
+        /// <see cref="MMDevice"/>.
+        /// </summary>
         public string FriendlyName => Device.FriendlyName;
 
+        /// <summary>
+        /// Method to convert the wrapped <see cref="MMDevice"/>
+        /// name correctly for use in the <see cref="ViewModels.AudioInterfaceViewModel"/>
+        /// combobox item.
+        /// </summary>
+        /// <returns></returns>
         public override string ToString() => Device.ToString();
     }
 }
--- TAC_COM/Models/PeakMeterWrapper.cs
+++ TAC_COM/Models/PeakMeterWrapper.cs
@@ -3,15 +3,26 @@ using TAC_COM.Models.Interfaces;
 
 namespace TAC_COM.Models
 {
-    public class PeakMeterWrapper() : IPeakMeterWrapper
+    /// <summary>
+    /// Wrapper class for an <see cref="audioMeterInformation"/>, to
+    /// faciliate easier testing.
+    /// </summary>
+    public class PeakMeterWrapper : IPeakMeterWrapper
     {
         private AudioMeterInformation? audioMeterInformation;
 
+        /// <summary>
+        /// Method to initialise the <see cref="AudioMeterInformation"/>
+        /// from a given <see cref="MMDevice"/>.
+        /// </summary>
+        /// <param name="device"> The device to read from.</param>
         public void Initialise(MMDevice device)
         {
             audioMeterInformation = AudioMeterInformation.FromDevice(device);
         }
 
+        /// Method to read the value of <see cref="audioMeterInformation"/>,
+        /// and convert to a percentage value for display in ui.
         public float GetValue()
         {
             return audioMeterInformation?.PeakValue * 100 ?? 0;
