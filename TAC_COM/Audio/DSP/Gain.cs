b08704769e0f253dc42eaeda20c83c215ed5b229
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-12-20 13:45:11 +0000

    Add XML documentation comments

--- TAC_COM/Audio/DSP/Gain.cs
+++ TAC_COM/Audio/DSP/Gain.cs
@@ -3,11 +3,20 @@ using TAC_COM.Audio.Utils;
 
 namespace TAC_COM.Audio.DSP
 {
+    /// <summary>
+    /// Basic adjustable gain control effect for use with an <see cref="ISampleSource"/>.
+    /// </summary>
+    /// <param name="inputSource"></param>
     public class Gain(ISampleSource inputSource) : ISampleSource
     {
         readonly ISampleSource source = inputSource;
 
         private float gainLinear;
+
+        /// <summary>
+        /// Gets or sets the gain adjustment of the signal in
+        /// decibels.
+        /// </summary>
         public float GainDB
         {
             get => LinearDBConverter.LinearToDecibel(gainLinear);
@@ -17,6 +26,14 @@ namespace TAC_COM.Audio.DSP
             }
         }
 
+        /// <summary>
+        /// Implementation of the <see cref="ISampleSource"/> Read method,
+        /// in which the effect is applied to the sample buffer.
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="offset"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
         public int Read(float[] buffer, int offset, int count)
         {
             int samples = source.Read(buffer, offset, count);
@@ -27,16 +44,28 @@ namespace TAC_COM.Audio.DSP
             return samples;
         }
 
+        /// <summary>
+        /// Implementation of the <see cref="ISampleSource"/> CanSeek
+        /// property.
+        /// </summary>
         public bool CanSeek
         {
             get { return source.CanSeek; }
         }
 
+        /// <summary>
+        /// Implementation of the <see cref="ISampleSource"/> WaveFormat
+        /// property.
+        /// </summary>
         public WaveFormat WaveFormat
         {
             get { return source.WaveFormat; }
         }
 
+        /// <summary>
+        /// Implementation of the <see cref="ISampleSource"/> Position
+        /// property.
+        /// </summary>
         public long Position
         {
             get
@@ -49,13 +78,22 @@ namespace TAC_COM.Audio.DSP
             }
         }
 
+        /// <summary>
+        /// Implementation of the <see cref="ISampleSource"/> Length
+        /// property.
+        /// </summary>
         public long Length
         {
             get { return source.Length; }
         }
 
+        /// <summary>
+        /// Implementation of the <see cref="ISampleSource"/> Dispose
+        /// method.
+        /// </summary>
         public void Dispose()
         {
+            GC.SuppressFinalize(this);
         }
     }
 
