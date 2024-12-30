eeec5d98fbe0f2ffe1ac15d938e7590e08bd1fca
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-12-30 14:01:38 +0000

    Add xml documentation comments

--- TAC_COM/Models/Profile.cs
+++ TAC_COM/Models/Profile.cs
@@ -4,9 +4,22 @@ using TAC_COM.Services.Interfaces;
 
 namespace TAC_COM.Models
 {
+    /// <summary>
+    /// Class that represents a preset configuration of settings,
+    /// including sfx, icons and processing parameters.
+    /// </summary>
+    /// <param name="profileName"> The string name of the <see cref="Profile"/>.</param>
+    /// <param name="fileIdentifier">The string file suffix associated with the <see cref="Profile"/>.</param>
+    /// <param name="theme">The <see cref="Uri"/> for the associated theme.</param>
+    /// <param name="icon">The icon associated with the profile.</param>
     public class Profile(string profileName, string fileIdentifier, Uri theme, System.Windows.Media.ImageSource icon) : IProfile
     {
         private ISFXFileService fileService = new SFXFileService("Static/SFX");
+
+        /// <summary>
+        /// Gets or sets the <see cref="ISFXFileService"/> used to#
+        /// load sfx files.
+        /// </summary>
         public ISFXFileService FileService
         {
             get => fileService;
@@ -17,6 +30,11 @@ namespace TAC_COM.Models
         }
 
         private string profileName = profileName;
+
+        /// <summary>
+        /// Gets or sets the string representing the profile
+        /// name.
+        /// </summary>
         public string ProfileName
         {
             get => profileName;
@@ -27,6 +45,16 @@ namespace TAC_COM.Models
         }
 
         private string fileIdentifier = fileIdentifier;
+
+        /// <summary>
+        /// Gets or sets the string filename suffix used
+        /// when loading sfx files.
+        /// </summary>
+        /// <remarks>
+        /// For example, a value of "GMS" will be used to attempt
+        /// to load the "GateOpenGMS.wav", the "GateCloseGMS.wav"
+        /// and the "NoiseGMS.wav".
+        /// </remarks>
         public string FileIdentifier
         {
             get => fileIdentifier;
@@ -37,6 +65,11 @@ namespace TAC_COM.Models
         }
 
         private Uri theme = theme;
+
+        /// <summary>
+        /// Gets or sets the <see cref="Uri"/> for the
+        /// theme's .xaml resource dictionary file.
+        /// </summary>
         public Uri Theme
         {
             get => theme;
@@ -47,6 +80,10 @@ namespace TAC_COM.Models
         }
 
         private System.Windows.Media.ImageSource icon = icon;
+
+        /// <summary>
+        /// Gets or sets the <see cref="Profile"/>'s icon image.
+        /// </summary>
         public System.Windows.Media.ImageSource Icon
         {
             get => icon;
@@ -57,6 +94,11 @@ namespace TAC_COM.Models
         }
 
         private EffectParameters settings = new();
+
+        /// <summary>
+        /// Gets or sets the value representing all the profile-specific
+        /// effects parameters.
+        /// </summary>
         public EffectParameters Settings
         {
             get => settings;
@@ -67,6 +109,11 @@ namespace TAC_COM.Models
         }
 
         private IFileSourceWrapper? noiseSource;
+
+        /// <summary>
+        /// Gets or sets the <see cref="IFileSourceWrapper"/>
+        /// representing the background noise sfx.
+        /// </summary>
         public IFileSourceWrapper? NoiseSource
         {
             get => noiseSource;
@@ -77,6 +124,11 @@ namespace TAC_COM.Models
         }
 
         private IFileSourceWrapper? openSFXSource;
+
+        /// <summary>
+        /// Gets or sets the <see cref="IFileSourceWrapper"/>
+        /// representing the mic click open sfx.
+        /// </summary>
         public IFileSourceWrapper? OpenSFXSource
         {
             get => openSFXSource;
@@ -87,6 +139,11 @@ namespace TAC_COM.Models
         }
 
         private IFileSourceWrapper? closeSFXSource;
+
+        /// <summary>
+        /// Gets or sets the <see cref="IFileSourceWrapper"/>
+        /// representing the mic click close sfx.
+        /// </summary>
         public IFileSourceWrapper? CloseSFXSource
         {
             get => closeSFXSource;
@@ -96,6 +153,9 @@ namespace TAC_COM.Models
             }
         }
 
+        /// <summary>
+        /// Method to load all sfx sources into memory, ready for playback.
+        /// </summary>
         public void LoadSources()
         {
             if (FileIdentifier != null)
@@ -115,6 +175,11 @@ namespace TAC_COM.Models
             }
         }
 
+        /// <summary>
+        /// Override method to return the <see cref="ProfileName"/>
+        /// as a string.
+        /// </summary>
+        /// <returns> The <see cref="ProfileName"/> string.</returns>
         public override string ToString()
         {
             return ProfileName ?? string.Empty;
