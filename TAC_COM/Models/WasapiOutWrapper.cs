2d9239249a61daeb0b6259f054c246b9d9777ebe
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-12-30 14:13:14 +0000

    Add xml documentation comments

--- TAC_COM/Models/WasapiOutWrapper.cs
+++ TAC_COM/Models/WasapiOutWrapper.cs
@@ -5,10 +5,17 @@ using TAC_COM.Models.Interfaces;
 
 namespace TAC_COM.Models
 {
+    /// <summary>
+    /// Wrapper class for the <see cref="CSCore.SoundOut.WasapiOut"/>,
+    /// to faciliate easier testing.
+    /// </summary>
     public class WasapiOutWrapper : IWasapiOutWrapper
     {
         private readonly WasapiOut wasapiOut = new() { Latency = 5 };
 
+        /// <summary>
+        /// Gets or sets the class's <see cref="MMDevice"/>.
+        /// </summary>
         public MMDevice Device
         {
             get => wasapiOut.Device;
@@ -18,6 +25,10 @@ namespace TAC_COM.Models
             }
         }
 
+        /// <summary>
+        /// Gets or sets the value representing the final output volume
+        /// of the <see cref="WasapiOut"/>.
+        /// </summary>
         public float Volume
         {
             get => wasapiOut.Volume;
@@ -27,18 +38,36 @@ namespace TAC_COM.Models
             }
         }
 
+        /// <summary>
+        /// Wrapper handler for the <see cref="WasapiOut.Stopped"/>
+        /// event handler.
+        /// </summary>
         public event EventHandler<PlaybackStoppedEventArgs> Stopped
         {
             add => wasapiOut.Stopped += value;
             remove => wasapiOut.Stopped -= value;
         }
 
+        /// <summary>
+        /// Method to manually dispose of the <see cref="WasapiOut"/>.
+        /// </summary>
         public void Dispose() => wasapiOut.Dispose();
 
+        /// <summary>
+        /// Method to initialise the <see cref="WasapiOut"/> for playback,
+        /// passing the <see cref="IWaveSource"/> to be played as a parameter.
+        /// </summary>
+        /// <param name="source">The <see cref="IWaveSource"/> to be played.</param>
         public void Initialize(IWaveSource? source) => wasapiOut.Initialize(source);
 
+        /// <summary>
+        /// Method to begin playback of the <see cref="WasapiOut"/>.
+        /// </summary>
         public void Play() => wasapiOut.Play();
 
+        /// <summary>
+        /// Method to stop playback of the <see cref="WasapiOut"/>.
+        /// </summary>
         public void Stop() => wasapiOut.Stop();
     }
 }
