3cf2b748d857af23461a0b6cd6e6ebf75d04ff2a
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-12-30 14:07:43 +0000

    Add xml documentation comments

--- TAC_COM/Models/WasapiCaptureWrapper.cs
+++ TAC_COM/Models/WasapiCaptureWrapper.cs
@@ -4,9 +4,17 @@ using TAC_COM.Models.Interfaces;
 
 namespace TAC_COM.Models
 {
+    /// <summary>
+    /// Wrapper class for the <see cref="WasapiCapture"/>, to facilitate
+    /// easier testing.
+    /// </summary>
     public class WasapiCaptureWrapper : IWasapiCaptureWrapper
     {
         private WasapiCapture wasapiCapture = new(false, AudioClientShareMode.Shared, 5);
+
+        /// <summary>
+        /// Gets or sets the current <see cref="CSCore.SoundIn.WasapiCapture"/>.
+        /// </summary>
         public WasapiCapture WasapiCapture
         {
             get => wasapiCapture;
@@ -16,6 +24,9 @@ namespace TAC_COM.Models
             }
         }
 
+        /// <summary>
+        /// Gets or sets the class's <see cref="MMDevice"/>.
+        /// </summary>
         public MMDevice Device
         {
             get => wasapiCapture.Device;
@@ -25,25 +36,45 @@ namespace TAC_COM.Models
             }
         }
 
+        /// <summary>
+        /// Wrapper handler for the <see cref="CSCore.SoundIn.WasapiCapture.DataAvailable"/>
+        /// event handler.
+        /// </summary>
         public event EventHandler<DataAvailableEventArgs> DataAvailable
         {
             add => wasapiCapture.DataAvailable += value;
             remove => wasapiCapture.DataAvailable -= value;
         }
 
+        /// <summary>
+        /// Wrapper handler for the <see cref="RecordingStoppedEventArgs"/>
+        /// event handler.
+        /// </summary>
         public event EventHandler<RecordingStoppedEventArgs> Stopped
         {
             add => wasapiCapture.Stopped += value;
             remove => wasapiCapture.Stopped -= value;
         }
 
+        /// <summary>
+        /// Method to manually dispose of the <see cref="WasapiCapture"/>.
+        /// </summary>
         public void Dispose() => wasapiCapture.Dispose();
 
+        /// <summary>
+        /// Method to initialise the <see cref="WasapiCapture"/>, ready
+        /// for recording.
+        /// </summary>
         public void Initialize() => wasapiCapture.Initialize();
 
+        /// <summary>
+        /// Method to start the <see cref="WasapiCapture"/> recording.
+        /// </summary>
         public void Start() => wasapiCapture.Start();
 
+        /// <summary>
+        /// Method to stop the <see cref="WasapiCapture"/> recording.
+        /// </summary>
         public void Stop() => wasapiCapture.Stop();
     }
-
 }
