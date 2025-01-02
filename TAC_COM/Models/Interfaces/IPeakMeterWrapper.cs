2cfc6002e611bad27e6e08657667da867614c143
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2025-01-02 14:47:19 +0000

    Move xml documentation to interface

--- TAC_COM/Models/Interfaces/IPeakMeterWrapper.cs
+++ TAC_COM/Models/Interfaces/IPeakMeterWrapper.cs
@@ -2,9 +2,21 @@
 
 namespace TAC_COM.Models.Interfaces
 {
+    /// <summary>
+    /// The interface that represents the wrapper class
+    /// for an <see cref="AudioMeterInformation"/>
+    /// </summary>
     public interface IPeakMeterWrapper
     {
+        /// <summary>
+        /// Method to initialise the <see cref="AudioMeterInformation"/>
+        /// from a given <see cref="MMDevice"/>.
+        /// </summary>
+        /// <param name="device"> The device to read from.</param>
         void Initialise(MMDevice device);
+
+        /// Method to read the value of <see cref="audioMeterInformation"/>,
+        /// and convert to a percentage value for display in ui.
         float GetValue();
     }
 }
--- TAC_COM/Models/PeakMeterWrapper.cs
+++ TAC_COM/Models/PeakMeterWrapper.cs
@@ -4,25 +4,19 @@ using TAC_COM.Models.Interfaces;
 namespace TAC_COM.Models
 {
     /// <summary>
-    /// Wrapper class for an <see cref="audioMeterInformation"/>, to
+    /// Wrapper class for an <see cref="AudioMeterInformation"/>, to
     /// faciliate easier testing.
     /// </summary>
     public class PeakMeterWrapper : IPeakMeterWrapper
     {
         private AudioMeterInformation? audioMeterInformation;
 
-        /// <summary>
-        /// Method to initialise the <see cref="AudioMeterInformation"/>
-        /// from a given <see cref="MMDevice"/>.
-        /// </summary>
-        /// <param name="device"> The device to read from.</param>
+        
         public void Initialise(MMDevice device)
         {
             audioMeterInformation = AudioMeterInformation.FromDevice(device);
         }
 
-        /// Method to read the value of <see cref="audioMeterInformation"/>,
-        /// and convert to a percentage value for display in ui.
         public float GetValue()
         {
             return audioMeterInformation?.PeakValue * 100 ?? 0;
