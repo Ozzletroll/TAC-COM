1a21549eda7d5f353ec82b92e96bad5539777424
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2025-01-03 15:09:59 +0000

    Move xml documentation to interface

--- TAC_COM/Services/Interfaces/IIconService.cs
+++ TAC_COM/Services/Interfaces/IIconService.cs
@@ -1,16 +1,49 @@
-﻿
-using System.Windows.Media;
+﻿using System.Windows.Media;
 
 namespace TAC_COM.Services.Interfaces
 {
+    /// <summary>
+    /// Interface representing the service responsible for invoking
+    /// system tray icon event handlers.
+    /// </summary>
     public interface IIconService
     {
+        /// <summary>
+        /// System tray icon change event. Occurs when the
+        /// <see cref="IconService"/> calls either
+        /// <see cref="SetLiveIcon"/>, <see cref="SetEnabledIcon"/>,
+        /// or <see cref="SetStandbyIcon"/>.
+        /// </summary>
         event EventHandler? ChangeSystemTrayIcon;
+
+        /// <summary>
+        /// Profile icon change event. Occurs when the 
+        /// <see cref="IconService"/> calls <see cref="SetActiveProfileIcon(System.Windows.Media.ImageSource)"/>.
+        /// </summary>
         event EventHandler? ChangeProfileIcon;
 
+        /// <summary>
+        /// Method to invoke the <see cref="ChangeProfileIcon"/> event.
+        /// </summary>
+        /// <param name="icon"> The new icon to pass as a <see cref="ProfileChangeEventArgs"/> parameter.</param>
         void SetActiveProfileIcon(ImageSource icon);
+
+        /// <summary>
+        /// Method to invoke the <see cref="ChangeSystemTrayIcon"/> event with the 
+        /// parameters required to set the "Enabled" state.
+        /// </summary>
         void SetEnabledIcon();
+
+        /// <summary>
+        /// Method to invoke the <see cref="ChangeSystemTrayIcon"/> event with the
+        /// parameters required to set the "Live" state.
+        /// </summary>
         void SetLiveIcon();
+
+        /// <summary>
+        /// Method to invokes the <see cref="ChangeSystemTrayIcon"/> event with the
+        /// parameters required to set the "Standby" state.
+        /// </summary>
         void SetStandbyIcon();
     }
 }
