490994691ec013a3ba8fde770ee5fe92cc97b7f2
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2025-01-05 01:06:13 +0000

    Add xml documentation comments

--- TAC_COM/Services/Interfaces/IMMDeviceEnumeratorService.cs
+++ TAC_COM/Services/Interfaces/IMMDeviceEnumeratorService.cs
@@ -3,9 +3,27 @@ using TAC_COM.Models.Interfaces;
 
 namespace TAC_COM.Services.Interfaces
 {
+    /// <summary>
+    /// Interface representing the service responsible for iterating
+    /// over and returning all connected input and output 
+    /// audio devices.
+    /// </summary>
     public interface IMMDeviceEnumeratorService
     {
+        /// <summary>
+        /// Method to return an observable collection
+        /// of all input devices, represented as
+        /// <see cref="IMMDeviceWrapper"/>s.
+        /// </summary>
+        /// <returns> An <see cref="ObservableCollection{T}"/> of <see cref="IMMDeviceWrapper"/>s.</returns>
         ObservableCollection<IMMDeviceWrapper> GetInputDevices();
+
+        /// <summary>
+        /// Method to return an observable collection
+        /// of all output devices, represented as
+        /// <see cref="IMMDeviceWrapper"/>s.
+        /// </summary>
+        /// <returns> An <see cref="ObservableCollection{T}"/> of <see cref="IMMDeviceWrapper"/>s.</returns>
         ObservableCollection<IMMDeviceWrapper> GetOutputDevices();
     }
 }
--- TAC_COM/Services/MMDeviceEnumeratorService.cs
+++ TAC_COM/Services/MMDeviceEnumeratorService.cs
@@ -6,10 +6,17 @@ using TAC_COM.Services.Interfaces;
 
 namespace TAC_COM.Services
 {
+    /// <summary>
+    /// Class responsible for iterating over and returning
+    /// all input and output devices.
+    /// </summary>
     public class MMDeviceEnumeratorService : IMMDeviceEnumeratorService
     {
         private readonly MMDeviceEnumerator enumerator;
 
+        /// <summary>
+        /// Initialises a new instance of the <see cref="MMDeviceEnumeratorService"/>.
+        /// </summary>
         public MMDeviceEnumeratorService()
         {
             enumerator = new MMDeviceEnumerator();
