199436da770f8efbd4267f729ffba883246742df
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2025-01-06 13:38:25 +0000

    Refactor and add xml documentation

--- TAC_COM/Services/Interfaces/ISFXFileService.cs
+++ TAC_COM/Services/Interfaces/ISFXFileService.cs
@@ -2,10 +2,47 @@
 
 namespace TAC_COM.Services.Interfaces
 {
+    /// <summary>
+    /// Interface representing the service responsible for 
+    /// returning the various sfx files in a format
+    /// ready for playback.
+    /// </summary>
     public interface ISFXFileService
     {
-        IWaveSource GetCloseSFX(string profile);
-        IWaveSource GetNoiseSFX(string profile);
+        /// <summary>
+        /// Method to return the mic click close sfx as an <see cref="IWaveSource"/> when
+        /// given a string file suffix.
+        /// </summary>
+        /// <remarks>
+        /// The file suffix should match the <see cref="Models.Profile.FileIdentifier"/>
+        /// of the current active <see cref="Models.Profile"/>.
+        /// </remarks>
+        /// <param name="fileSuffix"> The string file suffix of the desired file.</param>
+        /// <returns>The mic click close sfx file as an <see cref="IWaveSource"/>.</returns>
+        IWaveSource GetCloseSFX(string fileSuffix);
+
+        /// <summary>
+        /// Method to return the background noise sfx as an <see cref="IWaveSource"/> when
+        /// given a string file suffix.
+        /// </summary>
+        /// <remarks>
+        /// The file suffix should match the <see cref="Models.Profile.FileIdentifier"/>
+        /// of the current active <see cref="Models.Profile"/>.
+        /// </remarks>
+        /// <param name="fileSuffix"> The string file suffix of the desired file.</param>
+        /// <returns>The noise sfx file as an <see cref="IWaveSource"/>.</returns>
+        IWaveSource GetNoiseSFX(string fileSuffix);
+
+        /// <summary>
+        /// Method to return the mic click open sfx as an <see cref="IWaveSource"/> when
+        /// given a string file suffix.
+        /// </summary>
+        /// <remarks>
+        /// The file suffix should match the <see cref="Models.Profile.FileIdentifier"/>
+        /// of the current active <see cref="Models.Profile"/>.
+        /// </remarks>
+        /// <param name="fileSuffix"> The string file suffix of the desired file.</param>
+        /// <returns>The mic click open sfx file as an <see cref="IWaveSource"/>.</returns>
         IWaveSource GetOpenSFX(string fileSuffix);
     }
 }
--- TAC_COM/Services/SFXFileService.cs
+++ TAC_COM/Services/SFXFileService.cs
@@ -5,6 +5,11 @@ using TAC_COM.Services.Interfaces;
 
 namespace TAC_COM.Services
 {
+    /// <summary>
+    /// Class responsible for loading and returning sfx files
+    /// as <see cref="IWaveSource"/>'s, ready for playback.
+    /// </summary>
+    /// <param name="filepath"></param>
     public class SFXFileService(string filepath) : ISFXFileService
     {
         private readonly string sfxFilePath = filepath;
@@ -16,20 +21,26 @@ namespace TAC_COM.Services
             return CodecFactory.Instance.GetCodec(filename).ToMono();
         }
 
-        public IWaveSource GetCloseSFX(string profile)
+        public IWaveSource GetCloseSFX(string fileSuffix)
         {
-            var sfxName = "GateClose" + profile;
+            var sfxName = "GateClose" + fileSuffix;
             var filename = GetFile(sfxFilePath + "/GateClose", sfxName);
             return CodecFactory.Instance.GetCodec(filename).ToMono();
         }
 
-        public IWaveSource GetNoiseSFX(string profile)
+        public IWaveSource GetNoiseSFX(string fileSuffix)
         {
-            var sfxName = "Noise" + profile;
+            var sfxName = "Noise" + fileSuffix;
             var filename = GetFile(sfxFilePath + "/Noise", sfxName);
             return CodecFactory.Instance.GetCodec(filename).ToMono();
         }
 
+        /// <summary>
+        /// Static method to retrieve the path of a file within a specified folder.
+        /// </summary>
+        /// <param name="folder"> The name of the folder to search within.</param>
+        /// <param name="filename">The name of the file (without extension) to search for.</param>
+        /// <returns>The full path of the matching file if found; otherwise, <c>null</c></returns>
         private static string? GetFile(string folder, string filename)
         {
             string subfolderPath = Path.Combine(Directory.GetCurrentDirectory(), folder);
