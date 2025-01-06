95008eb8dfa8bd4110c77659ca8aeedad3ddd140
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2025-01-06 13:17:52 +0000

    Add xml documentation comments

--- TAC_COM/Services/Interfaces/ISettingsService.cs
+++ TAC_COM/Services/Interfaces/ISettingsService.cs
@@ -2,10 +2,30 @@
 
 namespace TAC_COM.Services.Interfaces
 {
+    /// <summary>
+    /// Interface representing the service responsible for updating 
+    /// and saving the config file.
+    /// </summary>
     public interface ISettingsService
     {
+        /// <summary>
+        /// Gets or sets the audio settings
+        /// <see cref="System.Configuration.ConfigurationSection"/>.
+        /// </summary>
         AudioSettings AudioSettings { get; set; }
+
+        /// <summary>
+        /// Gets or sets the keybind settings
+        /// <see cref="System.Configuration.ConfigurationSection"/>.
+        /// </summary>
         KeybindSettings KeybindSettings { get; set; }
+
+        /// <summary>
+        /// Method to update the config file when given a
+        /// string property name and the new value to update with.
+        /// </summary>
+        /// <param name="propertyName">The string name of the property to update.</param>
+        /// <param name="value">The new value to update the config with.</param>
         void UpdateAppConfig(string propertyName, object value);
     }
 }
--- TAC_COM/Services/SettingsService.cs
+++ TAC_COM/Services/SettingsService.cs
@@ -4,11 +4,41 @@ using TAC_COM.Settings;
 
 namespace TAC_COM.Services
 {
+    /// <summary>
+    /// Class responsible for updating the configuration file
+    /// used to store both audio and keybind settings.
+    /// </summary>
     public class SettingsService : ISettingsService
     {
+        /// <summary>
+        /// Gets or sets the configuration file for the application.
+        /// </summary>
         public Configuration AppConfig { get; private set; }
+
         public AudioSettings AudioSettings { get; set; }
         public KeybindSettings KeybindSettings { get; set; }
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="SettingsService"/>
+        ///  class, creating the configuration file and all required
+        ///  properties with default values.
+        /// </summary>
+        public SettingsService()
+        {
+            AppConfig = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+            if (AppConfig.Sections["AudioSettings"] is null)
+            {
+                AppConfig.Sections.Add("AudioSettings", new AudioSettings());
+            }
+            if (AppConfig.Sections["KeybindSettings"] is null)
+            {
+                AppConfig.Sections.Add("KeybindSettings", new KeybindSettings());
+            }
+
+            AudioSettings = (AudioSettings)AppConfig.GetSection("AudioSettings");
+            KeybindSettings = (KeybindSettings)AppConfig.GetSection("KeybindSettings");
+        }
 
         public void UpdateAppConfig(string propertyName, object value)
         {
@@ -34,23 +64,6 @@ namespace TAC_COM.Services
                 }
                 AppConfig.Save();
             }
-        }
-
-        public SettingsService()
-        {
-            AppConfig = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-
-            if (AppConfig.Sections["AudioSettings"] is null)
-            {
-                AppConfig.Sections.Add("AudioSettings", new AudioSettings());
-            }
-            if (AppConfig.Sections["KeybindSettings"] is null)
-            {
-                AppConfig.Sections.Add("KeybindSettings", new KeybindSettings());
-            }
-
-            AudioSettings = (AudioSettings)AppConfig.GetSection("AudioSettings");
-            KeybindSettings = (KeybindSettings)AppConfig.GetSection("KeybindSettings");
         }
     }
 }
