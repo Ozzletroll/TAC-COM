e0105cc8720d6ff851c15afcfd4a221c22fc345b
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2025-01-06 14:30:02 +0000

    Add xml documentation

--- TAC_COM/Services/Interfaces/IThemeService.cs
+++ TAC_COM/Services/Interfaces/IThemeService.cs
@@ -1,7 +1,17 @@
 ﻿namespace TAC_COM.Services.Interfaces
 {
+    /// <summary>
+    /// Interface representing the service responsible for
+    /// changing the theme of the app UI.
+    /// </summary>
     public interface IThemeService
     {
+        /// <summary>
+        /// Method to change the theme of the UI when given
+        /// a target theme URI.
+        /// </summary>
+        /// <param name="targetTheme">The <see cref="Uri"/> of the
+        /// target theme's resource dictionary.</param>
         public void ChangeTheme(Uri targetTheme);
     }
 }
--- TAC_COM/Services/ThemeService.cs
+++ TAC_COM/Services/ThemeService.cs
@@ -4,12 +4,30 @@ using TAC_COM.Services.Interfaces;
 
 namespace TAC_COM.Services
 {
+    /// <summary>
+    /// Class responsible for changing the theme of the app UI by
+    /// swapping resource dictionaries in <see cref="Application.Current"/>.
+    /// </summary>
+    /// <param name="_applicationContext"> The application context wrapper class.</param>
+    /// <param name="_uriService"> The <see cref="UriService"/> to utilise.</param>
     public class ThemeService(IApplicationContextWrapper _applicationContext, IUriService _uriService) : IThemeService
     {
         private readonly IApplicationContextWrapper applicationContext = _applicationContext;
         private readonly IUriService uriService = _uriService;
         private ResourceDictionary? currentTheme;
 
+        /// <inheritdoc/>
+        /// <remarks>
+        /// This method forces a refresh of the application's
+        /// resources by creating a new <see cref="ResourceDictionary"/>
+        /// and setting it as <see cref="IApplicationContextWrapper.Resources"/>.
+        /// Then, the target theme resources dictionary is added, updating
+        /// the theme colours of <see cref="AdonisUI"/>.
+        /// <para>
+        /// See the <see cref="AdonisUI"/> documentation for more infomation.
+        /// </para>
+        /// </remarks>
+        /// <param name="targetTheme"></param>
         public void ChangeTheme(Uri targetTheme)
         {
             // Force refresh of resources.xaml
