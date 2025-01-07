62b74774b017b7f947f09997463eb6fd8ea233a7
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2025-01-07 13:49:10 +0000

    Add xml documentation comments

--- TAC_COM/Services/Interfaces/IWindowService.cs
+++ TAC_COM/Services/Interfaces/IWindowService.cs
@@ -1,7 +1,15 @@
 ﻿namespace TAC_COM.Services.Interfaces
 {
+    /// <summary>
+    /// Interface representing the service responsible for opening
+    /// additional windows.
+    /// </summary>
     public interface IWindowService
     {
+        /// <summary>
+        /// Method to open a new instance of a <see cref="Views.KeybindWindowView"/>
+        /// as a dialog.
+        /// </summary>
         void OpenKeybindWindow();
     }
 }
--- TAC_COM/Services/WindowService.cs
+++ TAC_COM/Services/WindowService.cs
@@ -7,11 +7,26 @@ using TAC_COM.Views;
 
 namespace TAC_COM.Services
 {
+    /// <summary>
+    /// Class responsible for creating new dialog window views.
+    /// </summary>
+    /// <param name="_applicationContext"> The current application context wrapper.</param>
+    /// <param name="_keybindManager"> The <see cref="IKeybindManager"/> to pass to
+    /// the <see cref="KeybindWindowViewModel"/>.</param>
     public class WindowService(IApplicationContextWrapper _applicationContext, IKeybindManager _keybindManager) : IWindowService
     {
         private readonly IApplicationContextWrapper applicationContext = _applicationContext;
         private readonly KeybindManager keybindManager = (KeybindManager)_keybindManager;
         private KeybindWindowView? keybindWindow;
+
+        /// <summary>
+        /// Boolean value representing if the newly created
+        /// windows need to be shown.
+        /// </summary>
+        /// <remarks>
+        /// This of true by default. Set to false during
+        /// testing to prevent dialogs showing.
+        /// </remarks>
         public bool ShowWindow = true;
 
         public void OpenKeybindWindow()
