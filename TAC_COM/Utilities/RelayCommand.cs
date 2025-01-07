42ed256dfd4d8dc22e94765ff6174942b36820bb
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2025-01-07 14:37:22 +0000

    Add xml documentation

--- TAC_COM/Utilities/RelayCommand.cs
+++ TAC_COM/Utilities/RelayCommand.cs
@@ -2,22 +2,41 @@
 
 namespace TAC_COM.Utilities
 {
+    /// <summary>
+    /// Basic WPF ICommand implementation that relays it's functionality 
+    /// to other objects by invoking delegates.
+    /// </summary>
+    /// <param name="execute">The execution logic.</param>
+    /// <param name="canExecute">The execution status logic.</param>
+    /// <exception cref="ArgumentNullException">Thrown if the execute delegate is null.</exception>
     public class RelayCommand(Action<object?> execute, Func<object?, bool>? canExecute = null) : ICommand
     {
         private readonly Action<object?> execute = execute ?? throw new ArgumentNullException(nameof(execute));
         private readonly Func<object?, bool>? canExecute = canExecute;
 
+        /// <summary>
+        /// Occurs when changes occur that affect whether or not the command should execute.
+        /// </summary>
         public event EventHandler? CanExecuteChanged
         {
             add { CommandManager.RequerySuggested += value; }
             remove { CommandManager.RequerySuggested -= value; }
         }
 
+        /// <summary>
+        /// Defines the method that determines whether the command can execute in its current state.
+        /// </summary>
+        /// <param name="parameter">Data used by the command. If the command does not require data to be passed, this object can be set to null.</param>
+        /// <returns>true if this command can be executed; otherwise, false.</returns>
         public bool CanExecute(object? parameter)
         {
             return canExecute == null || canExecute(parameter);
         }
 
+        /// <summary>
+        /// Defines the method to be called when the command is invoked.
+        /// </summary>
+        /// <param name="parameter">Data used by the command. If the command does not require data to be passed, this object can be set to null.</param>
         public void Execute(object? parameter)
         {
             execute(parameter);
