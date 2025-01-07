409760d1b88c899c274d66d9c28ed60bae9e9ec6
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2025-01-07 14:23:33 +0000

    Add xml documentation

--- TAC_COM/Utilities/NotifyProperty.cs
+++ TAC_COM/Utilities/NotifyProperty.cs
@@ -2,10 +2,17 @@
 
 namespace TAC_COM.Utilities
 {
+    /// <summary>
+    /// Inheritable class to allow Property Change notification.
+    /// </summary>
     public class NotifyProperty : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler? PropertyChanged;
 
+        /// <summary>
+        /// Protected method to invoke the <see cref="PropertyChanged"/> event.
+        /// </summary>
+        /// <param name="propertyName">The string value representing the property name.</param>
         protected void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
