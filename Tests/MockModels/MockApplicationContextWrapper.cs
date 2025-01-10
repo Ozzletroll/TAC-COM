7a11aab4381eae1fb607593558f40f3433425d9c
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2025-01-10 16:20:13 +0000

    Add xml documentation

--- Tests/MockModels/MockApplicationContextWrapper.cs
+++ Tests/MockModels/MockApplicationContextWrapper.cs
@@ -3,11 +3,24 @@ using TAC_COM.Models.Interfaces;
 
 namespace Tests.MockModels
 {
+    /// <summary>
+    /// Mock class to act as the application context during testing.
+    /// </summary>
+    /// <param name="mainWindow"> The <see cref="MainWindow"/> to use.</param>
     public class MockApplicationContextWrapper(Window mainWindow) : IApplicationContextWrapper
     {
+        /// <summary>
+        /// Gets or sets the main window of the application 
+        /// context.
+        /// </summary>
         public Window MainWindow { get; set; } = mainWindow;
 
         private ResourceDictionary resources = [];
+
+        /// <summary>
+        /// Gets or sets the resource dictionary of the 
+        /// application context.
+        /// </summary>
         public ResourceDictionary Resources
         {
             get => resources;
