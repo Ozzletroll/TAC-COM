ef6d1e584257970916a6f037670b6838bf939388
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2025-01-15 15:26:26 +0000

    Remove redundant properties

--- Tests/MockModels/MockAudioManager.cs
+++ Tests/MockModels/MockAudioManager.cs
@@ -10,17 +10,13 @@ namespace Tests.MockModels
         public float NoiseGateThreshold { get; set; }
         public string NoiseGateThresholdString => NoiseGateThreshold.ToString();
         public float NoiseLevel { get; set; }
-        public string NoiseLevelString => NoiseLevel.ToString();
         public float OutputGainLevel { get; set; }
-        public string OutputGainLevelString => OutputGainLevel.ToString();
         public float OutputPeakMeterValue { get; set; }
         public bool State { get; set; }
         public ObservableCollection<IMMDeviceWrapper> InputDevices { get; set; } = [];
         public ObservableCollection<IMMDeviceWrapper> OutputDevices { get; set; } = [];
         public IProfile? ActiveProfile { get; set; }
         public float InterferenceLevel { get; set; }
-        public string InterferenceLevelString { get; }
-
         public void GetAudioDevices() { }
 
         public void SetInputDevice(IMMDeviceWrapper inputDevice) { }
