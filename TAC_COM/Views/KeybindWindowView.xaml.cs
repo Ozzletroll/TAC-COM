32c3cfd2abbd3aab783d61796c63ae9813ee7f57
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2025-01-20 11:56:36 +0000

    Refactor redundant switch case

--- TAC_COM/Views/KeybindWindowView.xaml.cs
+++ TAC_COM/Views/KeybindWindowView.xaml.cs
@@ -32,26 +32,20 @@ namespace TAC_COM.Views
         }
 
         /// <summary>
-        /// Method that prevents the user from moving the window
+        /// Method that prevents the user from moving the window.
         /// </summary>
         private IntPtr InterceptWindowMessages(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
         {
             const int WM_SYSCOMMAND = 0x0112;
             const int SC_MOVE = 0xF010;
 
-            switch (msg)
+            if (msg == WM_SYSCOMMAND)
             {
-                // Intercept window system messages
-                case WM_SYSCOMMAND:
-                    int command = wParam.ToInt32() & 0xfff0;
-                    // Handle move message, preventing window movement
-                    if (command == SC_MOVE)
-                    {
-                        handled = true;
-                    }
-                    break;
-                default:
-                    break;
+                int command = wParam.ToInt32() & 0xfff0;
+                if (command == SC_MOVE)
+                {
+                    handled = true;
+                }
             }
             return IntPtr.Zero;
         }
