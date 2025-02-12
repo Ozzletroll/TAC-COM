b3cc2552807e7b23cacbfb319f0a1be47f846dc8
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2025-02-12 00:12:53 +0000

    Add TestGetThemeRegistryValue

---
+++ Tests/UnitTests/ServiceTests/RegistryServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using TAC_COM.Services;
+
+namespace Tests.UnitTests.ServiceTests
+{
+    [TestClass]
+    public class RegistryServiceTests
+    {
+        [TestMethod]
+        public void TestGetThemeRegistryValue()
+        {
+            var registryService = new RegistryService();
+            var value = registryService.GetThemeRegistryValue();
+            Assert.IsNotNull(value);
+            Assert.IsTrue(value == 0 || value == 1);
+        }
+    }
+}
