69104cb1c526bef704fd3776f175fe10c8b1c4e7
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2025-02-13 15:08:05 +0000

    Add pre and post compression parallel effects

--- TAC_COM/Audio/EffectsChains/BaseChain.cs
+++ TAC_COM/Audio/EffectsChains/BaseChain.cs
@@ -9,16 +9,30 @@ namespace TAC_COM.Audio.EffectsChains
     {
         /// <summary>
         /// Method to return all <see cref="EffectReference"/>s to be
-        /// applied to the pre-distortion signal.
+        /// applied to the pre-compression primary signal.
         /// </summary>
         /// <returns> A list of all the <see cref="EffectReference"/>s.</returns>
-        public abstract List<EffectReference> GetPreDistortionEffects();
+        public abstract List<EffectReference> GetPreCompressionEffects();
 
         /// <summary>
         /// Method to return all <see cref="EffectReference"/>s to be
-        /// applied to the post-distortion signal.
+        /// applied to the post-compression primary signal.
         /// </summary>
         /// <returns> A list of all the <see cref="EffectReference"/>s.</returns>
-        public abstract List<EffectReference> GetPostDistortionEffects();
+        public abstract List<EffectReference> GetPostCompressionEffects();
+
+        /// <summary>
+        /// Method to return all <see cref="EffectReference"/>s to be
+        /// applied to the pre-compression parallel signal.
+        /// </summary>
+        /// <returns> A list of all the <see cref="EffectReference"/>s.</returns>
+        public abstract List<EffectReference> GetPreCompressionParallelEffects();
+
+        /// <summary>
+        /// Method to return all <see cref="EffectReference"/>s to be
+        /// applied to the post-compression parallel signal.
+        /// </summary>
+        /// <returns> A list of all the <see cref="EffectReference"/>s.</returns>
+        public abstract List<EffectReference> GetPostCompressionParallelEffects();
     }
 }
