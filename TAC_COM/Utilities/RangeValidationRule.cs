92bd5e2c2111a3266c161c21527b88766049f08d
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2025-02-21 15:23:37 +0000

    Add xml documentation

--- TAC_COM/Utilities/RangeValidationRule.cs
+++ TAC_COM/Utilities/RangeValidationRule.cs
@@ -3,11 +3,27 @@ using System.Windows.Controls;
 
 namespace TAC_COM.Utilities
 {
+    /// <summary>
+    /// Validation rule to check if a value is within a specified range.
+    /// </summary>
     public class RangeValidationRule : ValidationRule
     {
+        /// <summary>
+        /// Gets or sets the minimum value for the range.
+        /// </summary>
         public float Minimum { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum value for the range.
+        /// </summary>
         public float Maximum { get; set; }
 
+        /// <summary>
+        /// Validates the value to check if it is within the specified range.
+        /// </summary>
+        /// <param name="value"> The value to be validated.</param>
+        /// <param name="cultureInfo"> The <see cref="CultureInfo"/> to use when validating.</param>
+        /// <returns></returns>
         public override ValidationResult Validate(object value, CultureInfo cultureInfo)
         {
             if (value as string != null)
