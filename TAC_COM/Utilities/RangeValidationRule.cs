c29bcb52d30ba128fabb36c88e3f6dd2ea196a08
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2025-02-21 15:06:00 +0000

    Add RangeValidationRule

---
+++ TAC_COM/Utilities/RangeValidationRule.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace TAC_COM.Utilities
+{
+    public class RangeValidationRule : ValidationRule
+    {
+        public float Minimum { get; set; }
+        public float Maximum { get; set; }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value as string != null)
+            {
+                if (float.TryParse((string)value, out float number))
+                {
+                    if (number < Minimum || number > Maximum)
+                    {
+                        return new ValidationResult(false, $"Value should be between {Minimum} and {Maximum}.");
+                    }
+
+                    return ValidationResult.ValidResult;
+                }
+            }
+
+            return new ValidationResult(false, "Invalid value.");
+        }
+    }
+}
