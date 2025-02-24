30efd5f85148dfceb8cd858e6c508e1282fcac28
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2025-02-24 00:33:55 +0000

    Add PrefixValueConverterTests

---
+++ Tests/UnitTests/UtilityTests/PrefixValueConverterTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+using TAC_COM.Utilities.Converters;
+
+namespace Tests.UnitTests.UtilityTests
+{
+    [TestClass]
+    public class PrefixValueConverterTests
+    {
+        /// <summary>
+        /// Test method for the <see cref="PrefixValueConverter.Convert(object, Type, object, CultureInfo)"/> method.
+        /// </summary>
+        [TestMethod]
+        public void TestConvert()
+        {
+            var converter = new PrefixValueConverter();
+            var value = 5.0f;
+            var parameter = "+";
+            var culture = CultureInfo.InvariantCulture;
+
+            var result = converter.Convert(value, typeof(string), parameter, culture);
+
+            Assert.AreEqual("+5", result);
+        }
+
+        /// <summary>
+        /// Test method for the <see cref="PrefixValueConverter.ConvertBack(object, Type, object, CultureInfo)"/> method.
+        /// </summary>
+        [TestMethod]
+        public void TestConvertBack()
+        {
+            var converter = new PrefixValueConverter();
+            var value = "+5";
+            var parameter = "+";
+            var culture = CultureInfo.InvariantCulture;
+
+            var result = converter.ConvertBack(value, typeof(float), parameter, culture);
+
+            Assert.AreEqual(5.0f, result);
+        }
+    }
+}
