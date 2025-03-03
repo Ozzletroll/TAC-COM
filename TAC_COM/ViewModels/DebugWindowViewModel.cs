4a7b323eb95974a86c82434d588504f2d09cc919
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2025-03-03 20:59:22 +0000

    Add DebugWindowViewModel

---
+++ TAC_COM/ViewModels/DebugWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using TAC_COM.Models;
+
+namespace TAC_COM.ViewModels
+{
+    /// <summary>
+    /// Viewmodel exposing various debug properties to the <see cref="Views.DebugWindowView"/>.
+    /// </summary>
+    /// <param name="inputDeviceInfo"> DeviceInfo for the input device.</param>
+    /// <param name="outputDeviceInfo"> DeviceInfo for the output device.</param>
+    public class DebugWindowViewModel(DeviceInfo inputDeviceInfo, DeviceInfo outputDeviceInfo) : ViewModelBase
+    {
+        private DeviceInfo inputDevice = inputDeviceInfo;
+
+        /// <summary>
+        /// Gets or sets the <see cref="DeviceInfo"/> of
+        /// the input device.
+        /// </summary>
+        public DeviceInfo InputDevice
+        {
+            get => inputDevice;
+            set
+            {
+                inputDevice = value;
+            }
+        }
+
+        private DeviceInfo outputDevice = outputDeviceInfo;
+
+        /// <summary>
+        /// Gets or sets the <see cref="DeviceInfo"/> of
+        /// the output device.
+        /// </summary>
+        public DeviceInfo OutputDevice
+        {
+            get => outputDevice;
+            set
+            {
+                outputDevice = value;
+            }
+        }
+    }
+}
