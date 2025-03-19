13278a15b6402beb0ff01c60eebebbc87f65e977
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2025-03-19 11:19:51 +0000

    Fix missing xml documentation

--- TAC_COM/Utilities/Converters/ConverterInfo.cs
+++ TAC_COM/Utilities/Converters/ConverterInfo.cs
@@ -2,6 +2,10 @@
 
 namespace TAC_COM.Utilities.Converters
 {
+    /// <summary>
+    /// Class that contains a <see cref="IValueConverter"/> and its parameter,
+    /// for use in a <see cref="ValueConverterGroup"/>.
+    /// </summary>
     public class ConverterInfo
     {
         public required IValueConverter Converter { get; set; }
