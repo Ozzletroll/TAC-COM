508e038dda979d8d4fc4403a518e51705454f506
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2025-04-10 12:29:16 +0100

    Add ShowErrorMessage

--- TAC_COM/Services/DebugService.cs
+++ TAC_COM/Services/DebugService.cs
@@ -1,19 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TAC_COM.Services.Interfaces;
-
+﻿
 namespace TAC_COM.Services
 {
+    /// <summary>
+    /// Class responsible for formatting and showing error message dialogs.
+    /// </summary>
     public class DebugService
     {
-        private IWindowFactoryService windowFactoryService;
-
-        public void ShowErrorMessage()
+        /// <summary>
+        /// Static method to format a given Exception and display it as
+        /// an error message dialog.
+        /// </summary>
+        /// <param name="e"> The <see cref="Exception"/> to format and display.</param>
+        public static void ShowErrorMessage(Exception e)
         {
+            Dictionary<string, object> errorDict = [];
 
+            Dictionary<string, string?> outerExceptionDict = new()
+            {
+                { "Message", e.Message },
+                { "Source", e.Source },
+                { "Target Site", e.TargetSite?.ToString() },
+                { "Stack Trace", e.StackTrace },
+            };
+
+            errorDict.Add("Exception", outerExceptionDict);
+
+            Exception? currentException = e.InnerException;
+            int innerLevel = 1;
+            while (currentException != null)
+            {
+                Dictionary<string, string?> innerExceptionDict = new()
+                {
+                    { "Message", e.Message },
+                    { "Source", e.Source },
+                    { "Target Site", e.TargetSite?.ToString() },
+                    { "Stack Trace", e.StackTrace },
+                };
+
+                errorDict.Add($"Inner Exception {currentException}", innerExceptionDict);
+
+                currentException = currentException.InnerException;
+                innerLevel++;
+            }
+
+            WindowService.Instance.OpenErrorWindow(errorDict);
         }
     }
 }
