afb74680fb4c16a203ef6e22ca8b124033a85580
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2025-04-10 16:43:44 +0100

    Add ErrorWindowViewModel and View

---
+++ TAC_COM/ViewModels/ErrorWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿
+namespace TAC_COM.ViewModels
+{
+    /// <summary>
+    /// Viewmodel representing a formatted <see cref="Exception"/>, including
+    /// any nested inner exceptions.
+    /// </summary>
+    public class ErrorWindowViewModel(string exception) : ViewModelBase
+    {
+        private string error = exception;
+
+        public string Error
+        {
+            get => error;
+            set
+            {
+                error = value;
+                OnPropertyChanged(nameof(error));
+            }
+        }
+    }
+}
---
+++ TAC_COM/Views/ErrorWindowView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using AdonisUI.Controls;
+
+namespace TAC_COM.Views
+{
+    /// <summary>
+    /// Interaction logic for ErrorWindowView.xaml
+    /// </summary>
+    public partial class ErrorWindowView : AdonisWindow
+    {
+        public ErrorWindowView()
+        {
+            InitializeComponent();
+        }
+    }
+}
