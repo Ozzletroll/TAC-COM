aafe07a126c73007df52d488cd96cdcac11d528f
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2025-04-11 01:06:25 +0100

    Add TestErrorProperty

---
+++ Tests/UnitTests/ViewModelTests/ErrorWindowViewModelTests.cs
@@ -0,0 +1,20 @@
+﻿using TAC_COM.ViewModels;
+using Tests.Utilities;
+
+namespace Tests.UnitTests.ViewModelTests
+{
+    /// <summary>
+    /// Test class for the <see cref="ErrorWindowViewModelTests"/>.
+    /// </summary>
+    [TestClass]
+    public class ErrorWindowViewModelTests
+    {
+        [TestMethod]
+        public void TestErrorProperty()
+        {
+            var testViewModel = new ErrorWindowViewModel("Default string");
+            string newPropertyValue = "Test error string";
+            Utils.TestPropertyChange(testViewModel, nameof(testViewModel.Error), newPropertyValue);
+        }
+    }
+}
