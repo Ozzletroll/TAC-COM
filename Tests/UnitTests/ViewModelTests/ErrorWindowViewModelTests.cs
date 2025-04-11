bee16eb699a6fbdcfe1931ba87317f248cdf539c
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2025-04-11 01:57:32 +0100

    Add TestTerminateApplication

--- Tests/UnitTests/ViewModelTests/ErrorWindowViewModelTests.cs
+++ Tests/UnitTests/ViewModelTests/ErrorWindowViewModelTests.cs
@@ -21,5 +21,16 @@ namespace Tests.UnitTests.ViewModelTests
             string newPropertyValue = "Test error string";
             Utils.TestPropertyChange(testViewModel, nameof(testViewModel.Error), newPropertyValue);
         }
+
+        [TestMethod]
+        public void TestTerminateApplication()
+        {
+            var mockApplicationContextWrapper = new Mock<IApplicationContextWrapper>();
+            var testViewModel = new ErrorWindowViewModel(mockApplicationContextWrapper.Object, "Default string");
+
+            testViewModel.TerminateApplication.Execute(null);
+
+            mockApplicationContextWrapper.Verify(context => context.Shutdown(), Times.Once);
+        }
     }
 }
